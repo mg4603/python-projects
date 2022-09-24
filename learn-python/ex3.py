f4555c7ce91c795e2a76e30599db6c933bbb724e
Michael George <michael.g.george@gmail.com>
2022-09-24 15:37:27 +0530

    Complete ex3 learn-python-the-hard-way

---
+++ learn-python/ex3.py
@@ -0,0 +1,24 @@
+print("I will now count my chicken:")
+
+print("Hens %s" %(25+30/6))
+print("Roosters %s" %(100 - 25 * 3 % 4))
+
+print("Now I will count the egg:")
+
+print(3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6)
+
+print("Is it true that 3 + 2  < 5 - 7")
+print(3 + 2 < 5 - 7)
+
+print("What is 3 + 2? %s" % str(3 + 2))
+
+print("What is 5-7? %s" % str(5 - 7))
+
+print("Oh, that is why it's False")
+
+print("How about some more.")
+
+print("Is it greater? %s" % str(5 > -2))
+print("Is it greater or equal? %s" % (5 >= 2))
+print("Is it less or equal? %s" % (5 <= -2))
+
