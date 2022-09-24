c0060186d8d55c34c710cbe83867e00870e4153e
Michael George <michael.g.george@gmail.com>
2022-09-24 16:00:34 +0530

    Complete ex5 learn-python-the-hard-way

---
+++ learn-python/ex5.py
@@ -0,0 +1,17 @@
+# -- coding: utf-8 --
+name = "mg4603"
+age = 1005
+height = 195
+weight = 190  # lbs
+eye_color = "Red"
+teeth_color = "White"
+hair_color = "Black"
+
+print("Let's talk about %r" % name)
+print("He's %d inches tall" % height)
+print("He's %d pounds heavy" % weight)
+print("Actually that's not too heavy.")
+print(f"He's got {eye_color} eyes and {hair_color} hair.")
+print(f"His teeth are usually {teeth_color} depending on the coffee.")
+
+print(f"If I add {age}, {height} and {weight} I get {age + height + weight}")
