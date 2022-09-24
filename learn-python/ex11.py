017d76ea4109568cc5a36ecf22bd25523a59e7f8
Michael George <michael.g.george@gmail.com>
2022-09-25 04:57:14 +0530

    Complete ex11 learn-python-the-hard-way

---
+++ learn-python/ex11.py
@@ -0,0 +1,10 @@
+print("How old are you?", end=" ")
+age = input()
+
+print("How tall are you? (in cm)", end=" ")
+height = input()
+
+print("How much do you weigh? (in kg)", end=" ")
+weight = input()
+
+print(f"So, you're {age} years old, {height} centimeters tall and {weight} kilograms heavy.")
