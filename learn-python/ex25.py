2cc90454c0466e971ffa03da237e567e1142fc49
Michael George <michael.g.george@gmail.com>
2022-09-25 16:28:04 +0530

    Add fn to print last word in ex25 learn-python

--- learn-python/ex25.py
+++ learn-python/ex25.py
@@ -12,3 +12,7 @@ def print_first_word(words):
     word = words.pop(0)
     print(word)
 
+def print_last_word(words):
+    """Prints the last word after popping it off."""
+    word = words.pop(-1)
+    print(word)
