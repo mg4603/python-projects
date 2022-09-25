0ff63cee7ac6f9b5449dc5f0591f37c76ef2849d
Michael George <michael.g.george@gmail.com>
2022-09-25 05:38:14 +0530

    Complete ex13 inn learn-python-the-hard-way

---
+++ learn-python/ex13.py
@@ -0,0 +1,9 @@
+from sys import argv
+
+script, first, second, third = argv
+
+
+print(f"""The script is called: {script}
+Your first variable is: {first}
+Your second variable is: {second}
+Your third variable is: {third}""")
