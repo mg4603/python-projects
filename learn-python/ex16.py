34906adcf301523b1fb479cfdfa5ff92424764bf
Michael George <michael.g.george@gmail.com>
2022-09-25 07:28:07 +0530

    Complete ex16 learn-python-the-hard-way

---
+++ learn-python/ex16.py
@@ -0,0 +1,25 @@
+from sys import argv
+
+script, filename = argv
+
+print((f"We are going to erase the file {filename}"))
+print("If you don't want that, hit CTRL-C (^C).")
+print("If you want that, hit RETURN")
+
+input("?")
+
+print("Opening the file...")
+target = open(filename, "w")
+
+print("Truncating the file. Goodbye!")
+target.truncate()
+
+print("Now I'm going to ask you for three lines.")
+
+line1 = input("Line1: ")
+line2 = input("Line2: ")
+line3 = input("Line3: ")
+
+print("I'm going to write these to the file.")
+
+target.write(f"{line1}\n{line2}\n{line3}")
