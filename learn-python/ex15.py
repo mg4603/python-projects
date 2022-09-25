8f1c4657d634d056dd8744abe56e281655d18964
Michael George <michael.g.george@gmail.com>
2022-09-25 06:09:19 +0530

    Modify ex15 in learn-python-the-hard-way
    Modified code to close files after reading from them

--- learn-python/ex15.py
+++ learn-python/ex15.py
@@ -12,9 +12,13 @@ txt = open(file_name)
 print(f"Here's your file {file_name}:")
 print(txt.read())
 
+txt.close()
+
 print("Type the filename again:")
 file_again = input("> ")
 
 txt_again = open(file_again)
 
 print(txt_again.read())
+
+txt_again.close()
