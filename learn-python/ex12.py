cd867e3410851e728a39a425ea2861dc19937baa
Michael George <michael.g.george@gmail.com>
2022-09-25 05:30:09 +0530

    Complete ex12 in learn-python-the-hard-way

---
+++ learn-python/ex12.py
@@ -0,0 +1,5 @@
+age = input("How old are you? (in years)")
+height = input("How tall are you? (inn cm)")
+weight = input("How heavy are you? (in kg)")
+
+print(f"So, you are {age}years old, {height}cm tall and {weight}kg heavy")
