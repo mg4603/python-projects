a547b799cc3d00839d514213b491455e367f77f9
Michael George <michael.g.george@gmail.com>
2022-09-25 16:26:56 +0530

    Add fn sort_words to ex25 learn-python

--- learn-python/ex25.py
+++ learn-python/ex25.py
@@ -2,3 +2,7 @@ def break_words(stuff):
     """This function will break up words for us."""
     words = stuff.split(" ")
     return words
+
+def sort_words(words):
+    """Sorts the words."""
+    return sorted(words)
