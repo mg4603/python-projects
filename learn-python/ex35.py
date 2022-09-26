2ca1e68573b0d75f82ec0931e2ddad0d3b978b40
Michael George <michael.g.george@gmail.com>
2022-09-26 07:34:36 +0530

    Create a gold_room in ex35 in learn-python

--- learn-python/ex35.py
+++ learn-python/ex35.py
@@ -11,3 +11,17 @@ def isNum(num):
         return False
 
 
+def gold_room():
+    print("This room is full of gold. How much do you take?")
+
+    next = input(prompt)
+    if(isNum(next)):
+        how_much = int(next)
+    else:
+        dead("Man, learn to type a number.")
+    
+    if(how_much  < 50):
+        print("Nice, you're not greedy, you win!")
+        exit(0)
+    else:
+        dead("You greedy bastard!")
