14fa13c4fd203ac2584fabe2ec2b0c954b45684a
Michael George <michael.g.george@gmail.com>
2022-09-26 06:37:58 +0530

    Create fn with for loop ex33 learn-python

--- learn-python/ex33.py
+++ learn-python/ex33.py
@@ -16,4 +16,19 @@ def fn(end, step=1):
     for number in numbers:
         print(number)
 
-fn(6)
+def fn_with_for(end, step=1):
+    numbers = []
+    for i in range(0, end, step):
+        print("At the top i is %d" % i)
+
+        numbers.append(i)
+        i += step
+
+        print("Numbers now:", numbers)
+        print("At the bottom i is %d" %i)
+    
+    print("The numbers:")
+    for number in numbers:
+        print(number)
+
+fn_with_for(6, 2)
