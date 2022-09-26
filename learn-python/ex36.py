692ffcce150163924161ed15ae1018fe4765acef
Michael George <michael.g.george@gmail.com>
2022-09-26 13:19:02 +0530

    Create a swimming_pool in ex36 in learn_python

--- learn-python/ex36.py
+++ learn-python/ex36.py
@@ -1,11 +1,27 @@
 from sys import exit
 
+prompt = "> "
+
 def dead(reason):
-    print("You died by %s. Better luck next time!" % reason)
+    print("%s. Better luck next time!" % reason)
     exit(0)
 
 def win():
     print("Wow! You actually survived. How shocking!")
     exit(0)
+
+def swimming_pool():
+    print("""You are at the swimming pool.
+    There is a stranger in swimming in the pool.
+    Do you choose to have a swim or go to the jacuzzi?""")
+
+    next = input(prompt)
+
+    if("swim" in next):
+        dead("You were drowned by the deranged stranger.")
+    elif("jacuzzi" in next):
+        dead("You were eaten by the piranhas that some lunatics put in the jacuzzi")
+    else:
+        win()
 
 
