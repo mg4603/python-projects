83a0e43a2d55ba4185e8f234496c77276406b212
Michael George <michael.g.george@gmail.com>
2022-09-26 08:06:43 +0530

    Create a bear_room in ex35 in learn-python

--- learn-python/ex35.py
+++ learn-python/ex35.py
@@ -31,3 +31,28 @@ def gold_room():
         win("Nice, you're not greedy")
     else:
         dead("You greedy bastard!")
+
+
+def bear_room():
+    print("""There is a bear here.
+    The bear has a bunch of honey.
+    The fat bear is in front of another door.
+    How are you going to move the bear?""")
+    bear_moved = False
+
+    while True:
+        next = input(prompt)
+        
+        if(next == "take honey"):
+            dead("The bear looks at you, then slaps your face off.")
+        elif(next == "taunt bear" and not bear_moved):
+            print("The bear has moved from the door. You can go through it now.")
+            bear_moved = True
+        elif(next == "taunt bear" and bear_moved):
+            dead("The bear gets pissed off and chews your leg off.")
+        elif(next == "open door" and bear_moved):
+            gold_room()
+        else:
+            print("I've got no idea what that means.")
+
+bear_room()
