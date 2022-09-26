ee725530829d6fc4d64863e8fb830528394d30a8
Michael George <michael.g.george@gmail.com>
2022-09-26 06:57:21 +0530

    Complete ex34 learn-python

---
+++ learn-python/ex34.py
@@ -0,0 +1,10 @@
+animals = ["bear", "python", "peacock", "kangaroo", "whale", "platypus"]
+
+print("The animal at %s is %s" % ("1", animals[1]))
+print("The %s animal is %s" %("third", animals[2]))
+print("The %s animal is %s" %("fist", animals[0]))
+print("The animal at %s is %s" %("3", animals[3]))
+print("The %s animal is %s" %("fifth", animals[4]))
+print("The animal at %s is %s" %("2", animals[2]))
+print("The %s animal is %s" %("sixth", animals[5]))
+print("The animal at %s is %s" %("4", animals[4]))
