15d18caebbfea0882a47442c2df7cd50645f9e42
Michael George <michael.g.george@gmail.com>
2022-09-29 15:02:43 +0530

    Complete play fn in Engine class, ex43, learn-python

--- learn-python/ex43.py
+++ learn-python/ex43.py
@@ -76,7 +76,12 @@ class Engine:
         self.scene_map = scene_map
 
     def play(self):
-        pass
+        current_scene = self.scene_map.opening_scene()
+
+        while True:
+            print("\n-------------------------------------------")
+            next_scene_name = current_scene.enter()
+            current_scene = self.scene_map.next_scene(next_scene_name)
 
 
 class Death:
