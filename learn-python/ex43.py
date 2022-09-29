cb9a844a7a45211650c2ec5096ad40bd0f974251
Michael George <michael.g.george@gmail.com>
2022-09-29 12:51:03 +0530

    Add scene description of task in ex43 learn-python

--- learn-python/ex43.py
+++ learn-python/ex43.py
@@ -1,9 +1,31 @@
 """
+Text Description:
 Aliens have invaded a spaceship and our hero has to go through a maze of rooms 
 defeating them so he can escape into an escape pod to the planet below. The game
 will be more like a Zork or Adventure type game with text outputs and funny ways
 die. The game will involve an engine that runs a map full of rooms or scenes. 
 Each room will print it's own description when the player enters it and then tell
 the engine what room to run next out of the map.
+
+Scenes:
+    Death                   :   This is when the player dies and should be something
+                                funny.
+
+    Central Corridor        :   This is the starting point and has a gothon already.
+                                standing, which the player has to defeat with a joke 
+                                before continuing.
+
+    Laser Weapon Armory     :   This is where the player gets a neutron bomb to 
+                                blow up the ship before getting to the escape pod.
+                                It has a keypad he has to guess the number for.
+    
+    The Bridge              :   Another battle scene with a gothon where the player
+                                places the bomb.
+    
+    Escape Pod              :   Where the player escapes, but only after guessing the
+                                right escape pod.
+
+
+
 """
 
