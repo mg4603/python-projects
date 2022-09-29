8e9513e1cec873ffae22c0269ded237605e3d77a
Michael George <michael.g.george@gmail.com>
2022-09-29 12:40:18 +0530

    Add text description of task in ex43 learn-python

---
+++ learn-python/ex43.py
@@ -0,0 +1,9 @@
+"""
+Aliens have invaded a spaceship and our hero has to go through a maze of rooms 
+defeating them so he can escape into an escape pod to the planet below. The game
+will be more like a Zork or Adventure type game with text outputs and funny ways
+die. The game will involve an engine that runs a map full of rooms or scenes. 
+Each room will print it's own description when the player enters it and then tell
+the engine what room to run next out of the map.
+"""
+
