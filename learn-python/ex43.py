5102fa203d1ddef7963f65c978ed1888e4a52805
Michael George <michael.g.george@gmail.com>
2022-09-29 14:09:20 +0530

    Add Map class to ex43 in learn-python

--- learn-python/ex43.py
+++ learn-python/ex43.py
@@ -103,3 +103,15 @@ class EscapePod:
 
     def enter(self):
         pass
+
+
+class Map:
+
+    def __init__(self, start_scene):
+        pass
+
+    def next_scene(self, scene_name):
+        pass
+
+    def opening_scene(self):
+        pass
