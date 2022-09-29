ba56923d88c3e238874fe9f1c95fedf8a87afa16
Michael George <michael.g.george@gmail.com>
2022-09-30 04:40:19 +0530

    Complete collatz_sequence in functions inn automate

--- automate/functions.py
+++ automate/functions.py
@@ -5,3 +5,23 @@ def isNum(num):
     except ValueError:
         return False
 
+def collatz_sequence():
+
+    num = input("Enter an integer:\n")    
+    while(True):
+        if(isNum(num)):
+            num = int(num)
+            break
+        else:
+            print("Enter a number")
+            num = input()
+    
+    
+    while(num != 1):
+        if(num % 2 == 0):
+            num //= 2
+        else:
+            num = 3 * num + 1
+        print(num)
+
+collatz_sequence()
