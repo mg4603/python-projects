42c96d4e587595b00007240924f2816e45a1d818
Michael George <michael.g.george@gmail.com>
2022-09-30 08:54:48 +0530

    Create fn check_for_six_streak in lists in automate

--- automate/lists.py
+++ automate/lists.py
@@ -12,6 +12,19 @@ from random import randint
 def hundred_flips():
     return [randint(0,1) for i in range(100)]
 
+def check_for_six_streak(row):
+    num_of_streaks = 0
+    streak_length = 1
+    for i in range(1, len(row)):
+        if(row[i] == row(i-1)):
+            streak_length += 1
+        else:
+            streak_length = 1
+        
+        if(streak_length == 6):
+            num_of_streaks += 1
+            streak_length = 1
+    return num_of_streaks
 
 
 print(comma_code(['apples', 'bananas', 'tofu', 'cats']))
