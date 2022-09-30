3bf6317d107dc965184057c4a98133d93e4cd172
Michael George <michael.g.george@gmail.com>
2022-09-30 09:06:00 +0530

    Complete coin_flip_streaks in lists in automate

--- automate/lists.py
+++ automate/lists.py
@@ -8,6 +8,7 @@ def comma_code(spam):
     return "%s and %s" %(", ".join(spam[:-1]), spam[-1])
 
 from random import randint
+from tempfile import tempdir
 
 def hundred_flips():
     return [randint(0,1) for i in range(100)]
@@ -26,6 +27,15 @@ def check_for_six_streak(row):
             streak_length = 1
     return num_of_streaks
 
+def coin_flip_streaks():
+    total_num_of_streaks = 0
+    for _ in range(10000):
+        temp_row = hundred_flips()
+        total_num_of_streaks += check_for_six_streak(temp_row)
+
+    return total_num_of_streaks/ 10000 
+
+print(coin_flip_streaks())
 
 print(comma_code(['apples', 'bananas', 'tofu', 'cats']))
 
