51cbe738d0d345537885f59fcdc0587fd25be484
Michael George <michael.g.george@gmail.com>
2022-10-01 16:52:25 +0530

    Add fn to do so in bullet_point_adder, automate

--- automate/bullet_point_adder.py
+++ automate/bullet_point_adder.py
@@ -6,8 +6,14 @@ front of each line and paste it back to the clipboard
 
 from pyperclip import copy, paste
 
+def bullet_point_adder(text):
+    lines = text.split("\n")
+    result = ""
+    for line in lines:
+        result += "* %s\n" % line
+    return result
 
 if __name__ == "__main__":
     text = paste()
-    bullet_point_adder(text)
+    text = bullet_point_adder(text)
     copy(text)
