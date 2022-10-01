399141afbc9bf42e30742565eab50cb42a240c3c
Michael George <michael.g.george@gmail.com>
2022-10-01 16:21:26 +0530

    Add main to mclip in automate

--- automate/mclip.py
+++ automate/mclip.py
@@ -2,15 +2,28 @@
 """
 mclip.py - A multi-clipboard program
 """
+from argparse import ArgumentParser
+from sys import exit
+from pyperclip import copy
 
 TEXT = {"agree" : """Yes, I agree. That sounds fine to me.""",
         "busy"  : """Sorry, can we do this later this week or next week?""",
         "upsell": """Would you consider making this a monthly donation?"""
         }
-from argparse import ArgumentParser
 
 def parse_args():
     parser = ArgumentParser()
-    parser.add_argument("key-phrase", help="copy-phrase's key")
+    parser.add_argument("key", help="copy-phrase's key")
     return parser.parse_args()
+
+
+if __name__ == "__main__":
+    args = parse_args()
+    key_phrase = args.key
+
+    if(key_phrase in TEXT):
+        copy(TEXT[key_phrase])
+        print("Text for %s copied to clipboard" % key_phrase)
+    else:
+        print("There is no text for %s" % key_phrase)
 
