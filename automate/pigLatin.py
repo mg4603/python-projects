4c204f344c222ed0790d229475561e7468ced4f8
Michael George <michael.g.george@gmail.com>
2022-10-01 18:28:04 +0530

    Add main to pigLatin in automate

---
+++ automate/pigLatin.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""
+Program to convert english text to pig latin
+"""
+
+if __name__ == "__main__":
+    message = input("Enter the english message to translate to pig latin:\n")
+    print(convert(message))
