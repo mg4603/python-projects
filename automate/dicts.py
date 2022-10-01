a639c9feb1ac688384e7420e6e70295c3acb1386
Michael George <michael.g.george@gmail.com>
2022-10-01 09:52:46 +0530

    Add fn to check if key exists in dict in automate

--- automate/dicts.py
+++ automate/dicts.py
@@ -1,3 +1,5 @@
+from ast import excepthandler
+import re
 from sys import exit
 
 def invalid_board(reason):
@@ -11,15 +13,9 @@ def isNum(num):
     except ValueError:
         return False
 
-def isValidChessboard(board):
-    board_nums = ['1', '2', '3', '4', '5', '6', '7', '8']
-    board_alphs = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h']
-    pieces =["pawn", "queen", "bishop", "king", "rook", "knight"]
-    colors = ["w", "b"]
-    if(type(board) != dict):
-        invalid_board("Board has to be a dict of positions and pieces")
-    for key, value in board.items():
-        if(len(key) > 2):
-            invalid_board("Invalid position")
-        num, letter = key.split("")
-        if()
+def keyExists(dictionary, key):
+    try:
+        dictionary[key]
+        return True
+    except KeyError:
+        return False
