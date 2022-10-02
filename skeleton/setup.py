c68f1adb95934b1c53c8662ca25f782d3d196f29
Michael George <michael.g.george@gmail.com>
2022-10-03 05:18:59 +0530

    Add config info to setup.py in skeleton

--- skeleton/setup.py
+++ skeleton/setup.py
@@ -2,3 +2,18 @@ try:
     from setuptools import setup
 except ImportError:
     from distutils.core import setup
+
+
+
+config = {
+    "description": "Project description",
+    "author": "mg4603",
+    "url": "URL to get it at",
+    "download_url": "Where to download it",
+    "author_email": "mgeo4603@gmail.com",
+    "version": "0.1",
+    "install_requires": ['nose'],
+    "packages": ["NAME"],
+    "scripts":  [],
+    "name": "project name"
+}
