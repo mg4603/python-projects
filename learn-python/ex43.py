6b71b1e12a5ca2bdeff2efb37195a8683898ab20
Michael George <michael.g.george@gmail.com>
2022-10-02 13:14:00 +0530

    Complete enter fn for EscapePod in ex43 in learn-python

--- learn-python/ex43.py
+++ learn-python/ex43.py
@@ -219,7 +219,36 @@ class Bridge:
 class EscapePod:
 
     def enter(self):
-        pass
+        print("You rush through the ship, desperately trying to make it")
+        print("to the escape pod before the whole ship explodes. It seems")
+        print("like hardly any Gothons are on the ship, so your run is ")
+        print("clear of interference. You get to the chamber with escape")
+        print("pods, and now need to pick one to take. Some of them could")
+        print("be damaged but you don't have time to look. There's 5 pods,")
+        print("which one do you take?")
+
+        good_pod = randint(1, 5)
+        guess = input("[pod#]> ")
+
+
+        while(not isNum(guess)):
+            print("Learn to type a number.")
+            guess = input("[pod#]> ")
+
+        if(int(guess) != good_pod):
+            print("You jump into pod %s and hit the eject button." % guess)
+            print("The pod escapes out into the void of space, then")
+            print("implodes as the hull ruptures, crushing your body")
+            print("into jam jelly.")
+            return "death"
+        else:
+            print("You jump into pod %s and hit the eject button." % guess)
+            print("The pod easily slides out into space heading to the")
+            print("planet below. As it flies to the planet, you look")
+            print("back and see your ship implode and explode like a ")
+            print("bright star, taking out the Gothon ship at the same time.")
+            print("You won!")
+            return "finished"
 
 
 class Map:
