f08b1386e1194fb6a9625c8c470f55b3a4959a08
Michael George <michael.g.george@gmail.com>
2022-10-03 17:23:41 +0530

    Define Sentence in parser in ex48, learn-python

--- learn-python/ex48/ex48/parser.py
+++ learn-python/ex48/ex48/parser.py
@@ -1,2 +1,9 @@
 class ParserError(Exception):
     pass
+
+class Sentence():
+
+    def __init__(self, subj, verb, obj):
+        self.subj = subj[1]
+        self.verb = verb[1]
+        self.obj = obj[1]
