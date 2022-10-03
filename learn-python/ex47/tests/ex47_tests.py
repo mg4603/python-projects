52fdcc2677bbd0ae481d9854a640d139b7f2b37b
Michael George <michael.g.george@gmail.com>
2022-10-03 08:22:09 +0530

    Create test for Room in ex47_tests in learn-python

--- learn-python/ex47/tests/ex47_tests.py
+++ learn-python/ex47/tests/ex47_tests.py
@@ -1,11 +1,9 @@
-from nose import *
-import ex47
+from nose.tools import *
+from ex47.game import Room
 
-def setup():
-    print("SETUP!")
-
-def teardown():
-    print("TEAR DOWN!")
-
-def test_basic():
-    print("I RAN!")
+def test_room():
+    gold = Room("GoldRoom",
+                """This room has gold in it you can grab. There's a door
+                to the north.""")
+    assert_equal(gold.name, "GoldRoom")
+    assert_equal(gold.paths, {})
