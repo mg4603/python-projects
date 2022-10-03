4c3eeccd4fedc021d92b0b3f78d94d0f4efccf62
Michael George <michael.g.george@gmail.com>
2022-10-03 14:21:57 +0530

    Modify test_directions in ex48_tests in learn-python

--- learn-python/ex48/tests/ex48_tests.py
+++ learn-python/ex48/tests/ex48_tests.py
@@ -1,9 +1,9 @@
 from nose.tools import *
-from ex48 import lexicon
+from ex48.lexicon import scan
 
 def test_directions():
-    assert_equal(lexicon.scan("north"), [("direction", "north")])
-    result = lexicon.scan("north south east")
+    assert_equal(scan("north"), [("direction", "north")])
+    result = scan("north south east")
     assert_equal(result, [("direction", "north"),
                           ("direction", "south")
                           ("direction", "east")])
