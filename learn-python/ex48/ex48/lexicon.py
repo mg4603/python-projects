30a4d4d610fee886e0e3603393bf60dd637503d3
Michael George <michael.g.george@gmail.com>
2022-10-03 14:48:13 +0530

    Add scanner lexicon to lexicon, ex47, learn-python

--- learn-python/ex48/ex48/lexicon.py
+++ learn-python/ex48/ex48/lexicon.py
@@ -1,2 +1,13 @@
+directions = ["north", "south", "east", "west", 
+              "up", "down", "left", "right", "back"]
+
+verbs = ["go", "stop", "kill", "eat"]
+
+stop_words = ["the", "in", "of", "from", "at", "it"]
+
+nouns = ["door", "bear", "princess", "cabinet"]
+
+
+
 def scan():
     pass
