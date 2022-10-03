440139277637f6b66e866c28653a6dab771804e0
Michael George <michael.g.george@gmail.com>
2022-10-04 03:57:11 +0530

    Test skip in parser in ex48 in learn-python

--- learn-python/ex48/tests/parser_tests.py
+++ learn-python/ex48/tests/parser_tests.py
@@ -7,6 +7,18 @@ def test_peek():
     assert_equal(peek(scan("bear")), "noun")
 
 def test_match():
-    assert_equal(match(scan("kill the bear"), "verb"), ("verb", "kill"))
-    assert_equal(match(scan("kill the bear"), "noun"), None)
+    assert_equal(match(scan("kill the bear"), "verb"), 
+                 ("verb", "kill"))
+    assert_equal(match(scan("kill the bear"), "noun"), 
+                 None)
     assert_equal(match(scan(""), "noun"), None)
+
+def test_skip():
+    word_list = [("stop", "the"),
+                 ("noun", "player"),
+                 ("verb", "kill"),
+                 ("stop", "the"),
+                 ("noun", "bear")]
+    skip(word_list, "stop")
+    assert_equal(match(word_list, peek(word_list)),
+                 ("noun", "player"))
