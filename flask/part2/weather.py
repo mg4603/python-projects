af1186f08b64f66cdfe6c8ec273ff91015fbea99
Michael George <michael.g.george@gmail.com>
2022-10-05 13:13:36 +0530

    Define root route in weather.py in part2, flask

--- flask/part2/weather.py
+++ flask/part2/weather.py
@@ -3,14 +3,19 @@ from flask import Flask
 
 def openweather_api():
     config = ConfigParser()
-    config.read("../config/keys_config.cfg")
+    config.read("../../config/keys_config.cfg")
     return config.get("openweather", "api_key")
 
 API_KEY = openweather_api()
 
 app = Flask(__name__)
 
-
+@app.route("/")
+def hello():
+    return f"""
+    <h1>Hello Gators!<h1>
+    <p><a href="/user/Lol">Click me!</a></p>
+    """
 
 if __name__ == "__main__":
-    app.runs(debug=True)
+    app.run(debug=True)
