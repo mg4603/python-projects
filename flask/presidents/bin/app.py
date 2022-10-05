bbe8b487b25bb57151c7c19923aeeb98b289d7ae
Michael George <michael.g.george@gmail.com>
2022-10-05 15:33:26 +0530

    Define user route in presidents flask project

--- flask/presidents/bin/app.py
+++ flask/presidents/bin/app.py
@@ -2,5 +2,10 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+@app.route("/user/<name>")
+def user(name):
+    return render_template("hello.html", name=name)
+
+    
 if __name__ == "__main__":
     app.run(debug=True)
