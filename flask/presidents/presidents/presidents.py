b9aa6a76775aa8ca9d4b82adbe5c82cc33788570
Michael George <michael.g.george@gmail.com>
2022-10-05 20:56:19 +0530

    Add fn to make_ordinal in presidents.py flask

--- flask/presidents/presidents/presidents.py
+++ flask/presidents/presidents/presidents.py
@@ -8,3 +8,14 @@ def isInt(integer):
     except ValueError:
         return False
 
+def make_ordinal(num):
+    """
+    Convert an integer into its ordinal representation
+    """
+    if(isInt(num)):
+        num = int(num)
+    
+    if 11 <= (num%100) <= 13:
+        return str(num)+"th"
+    else:
+        return str(num)+["th", "st", "nd", "rd", "th"][min(num%10, 4)]
