fccafdb1152d664384ed504357cc17172c6d78af
Michael George <michael.g.george@gmail.com>
2022-10-06 20:53:03 +0530

    Setup app factory for forms flaskr app

---
+++ flask/forms/flaskr/__init__.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template
+from os.path import join
+from os import makedirs
+
+def create_app(test_config=None):
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_mapping(
+        SECRET_KEY="dev",
+        DATABASE=join(app.instance_path, "flaskr.sqlite"),
+    )
+
+    if test_config is None:
+        # load instance config, if it exists, when not testing
+        app.config.from_pyfile("config.py", silent=True)
+    else:
+        # load test config if passed in
+        app.config.from_mapping(test_config)
+    
+    # ensure the instance folder exists
+    try:
+        makedirs(app.instance_path)
+    except OSError:
+        pass
+    
+    
+    return app
