2a5589a577cf5988d5ae3782ca1465e36f8f2951
Michael George <michael.g.george@gmail.com>
2022-10-07 14:42:59 +0530

    Add fn to init_db in views flask app

--- flask/views/app/db.py
+++ flask/views/app/db.py
@@ -17,3 +17,12 @@ def close_db(e=None):
 
     if db is not None:
         db.close()
+
+
+def init_db():
+    db = get_db()
+
+    with current_app.open_resource("schema.sql") as f:
+        db.executescript(f.read().decode("utf-8"))
+    
+    
