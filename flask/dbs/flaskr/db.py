5ead1029d7c93cea3c90bfe3b29f759c9071e465
Michael George <michael.g.george@gmail.com>
2022-10-07 13:59:23 +0530

    Fix error in close_db in dbs flask app

--- flask/dbs/flaskr/db.py
+++ flask/dbs/flaskr/db.py
@@ -12,7 +12,7 @@ def get_db():
 
         return g.db
 
-def close_db():
+def close_db(e=None):
     db = g.pop("db", None)
 
     if db is not None:
@@ -33,4 +33,3 @@ def init_db_command():
 def init_app(app):
     app.teardown_appcontext(close_db)
     app.cli.add_command(init_db_command)
-    
