233628b66e3424b2eaef9f410308b15004879624
Michael George <michael.g.george@gmail.com>
2022-10-07 15:30:11 +0530

    Add init-db command to flask in views flask app

--- flask/views/app/db.py
+++ flask/views/app/db.py
@@ -18,11 +18,14 @@ def close_db(e=None):
     if db is not None:
         db.close()
 
-
 def init_db():
     db = get_db()
 
     with current_app.open_resource("schema.sql") as f:
         db.executescript(f.read().decode("utf-8"))
     
-    
+@command("init-db")
+def init_db_command():
+    """Clear existing data and create new tables"""
+    init_db()
+    echo("Initialized the database")
