ad06515dd73cc506be51d9e3f3645149335ff421
Michael George <michael.g.george@gmail.com>
2022-10-07 14:28:13 +0530

    Setup app factory for views flask app

---
+++ flask/views/app/__init__.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template
+from os import makedirs, urandom
+from os.path import join
+
+def create_app(test_config=None):
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_mapping(
+        SECRET_KEY=urandom(24).hex(),
+        DATABASE=join(app.instance_path, "flaskr.sqlite"),
+    )
+
+    if test_config is None:
+        app.config.from_pyfile("config.py")
+    else:
+        app.config.from_mapping(test_config)
+    
+    try:
+        makedirs(app.instance_path)
+    except OSError:
+        pass
+
+    return app
