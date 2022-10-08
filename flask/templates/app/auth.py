be9a5a7fa7f2c85c0bd893fb0d9c27eeefbf3af8
Michael George <michael.g.george@gmail.com>
2022-10-08 10:15:49 +0530

    Create login view in auth_bp in templates app

--- flask/templates/app/auth.py
+++ flask/templates/app/auth.py
@@ -31,7 +31,34 @@ def register():
                 error = f"User {username} already exists."
             else:
                 return redirect(url_for("auth.login"))
-                
+
         flash(error)
 
     return render_template("auth/register.html")
+
+@bp.route("/login", methods=("GET", "POST"))
+def login():
+    if request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
+        db = get_db()
+        error = None
+        
+        if username is None or password is None:
+            error = "Username and password are required"
+        else:
+            user = db.execute(
+                "SELECT * FROM user where username = ?", (username,)
+            ).fetchone()
+
+            if (user is None) or (check_password_hash(user["passowrd"], password)):
+                error = "Invalid credentials"
+            
+        if error is None:
+            session.clear()
+            session["user_id"] = user["id"]
+            return redirect(url_for("index"))
+        
+        flash(error)
+        
+    return render_template("auth/login.html")
