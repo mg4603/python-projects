e9b6658cca548981ca239d34bfe998e25aa1473e
Michael George <michael.g.george@gmail.com>
2022-10-08 10:06:46 +0530

    Create register view in auth_bp in templates app

--- flask/templates/app/auth.py
+++ flask/templates/app/auth.py
@@ -6,3 +6,32 @@ from flask import (
 )
 
 bp = Blueprint("auth", __name__, url_prefix="/auth")
+
+@bp.route("/register", methods=("GET", "POST"))
+def register():
+    if request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
+        db = get_db()
+        error = None
+
+        if not username:
+            error = "Username required"
+        elif not password:
+            error = "Password required"
+        
+        if error is None:
+            try:
+                db.execute(
+                    "INSERT INTO user (username, password) VALUES (?, ?)",
+                    (username, generate_password_hash(password)),
+                )
+                db.commit()
+            except db.IntegrityError:
+                error = f"User {username} already exists."
+            else:
+                return redirect(url_for("auth.login"))
+                
+        flash(error)
+
+    return render_template("auth/register.html")
