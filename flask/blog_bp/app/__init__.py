ca442ecd56f407bdbe190b526283fe5441e3b3aa
Michael George <michael.g.george@gmail.com>
2022-10-08 19:11:41 +0530

    Create app factory for blog_bp flask app

---
+++ flask/blog_bp/app/__init__.py
@@ -0,0 +1,22 @@
+from flask import Flask
+from os.path import join
+from os import urandom, makedirs
+
+def create_app(test_config=None):
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_mapping(
+        SECRET_KEY=urandom(24).hex(),
+        DATABASE=join(app.instance_path, "app.sqlite")
+    )
+
+    if test_config is None:
+        app.config.from_pyfile("config.py", silent=True)
+    else:
+        app.config.from_mapping(test_config)
+    
+    try:
+        makedirs(app.instance_path)
+    except OSError:
+        pass
+
+    return app
