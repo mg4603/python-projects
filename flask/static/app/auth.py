23a6ac485aa17966a707e41d163872cccf1a5f8f
Michael George <michael.g.george@gmail.com>
2022-10-08 16:14:17 +0530

    Add register route to auth_bp in static flask app

--- flask/static/app/auth.py
+++ flask/static/app/auth.py
@@ -3,7 +3,35 @@ from flask import(
     g, url_for, Blueprint, flash, redirect, render_template, request,
     session
 )
-from werkzeug.security import check_password_has, get_password_hash
+from werkzeug.security import check_password_has, generate_password_hash
 from .db import get_db
 
 bp = Blueprint("auth", __name__, url_prefix= "/auth")
+
+@bp.route("/register", methods=("GET", "POST"))
+def register():
+    if request.method == "POST":
+        username  = request.form["username"]
+        password = request.form["password"]
+        db = get_db()
+        error = None
+
+        if not username or not password:
+            error = "Username and password are required"
+        
+        if error is None:
+            try:
+                db.execute(
+                    "INSERT INTO user (username, password) VALUE (?, ?)", 
+                    (username, generate_password_hash(password))
+                )
+                db.commit()
+            except db.IntegrityError:
+                error = f"Username {username} already exists."
+            else:
+                return redirect(url_for("auth.login"))
+
+        flash(error)
+    
+    return render_template("auth/register.html")
+
