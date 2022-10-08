6a8dc02754c86b1f8778229f4b29cb665945980c
Michael George <michael.g.george@gmail.com>
2022-10-08 16:28:22 +0530

    Add login route to auth_bp in static flask app

--- flask/static/app/auth.py
+++ flask/static/app/auth.py
@@ -1,9 +1,11 @@
 from functools import wraps
+from tabnanny import check
+from tkinter.tix import Select
 from flask import(
     g, url_for, Blueprint, flash, redirect, render_template, request,
     session
 )
-from werkzeug.security import check_password_has, generate_password_hash
+from werkzeug.security import check_password_hash, generate_password_hash
 from .db import get_db
 
 bp = Blueprint("auth", __name__, url_prefix= "/auth")
@@ -35,3 +37,29 @@ def register():
     
     return render_template("auth/register.html")
 
+@bp.route("/login", methods=("GET", "POST"))
+def login():
+    if request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
+        db = get_db()
+        error = None
+        
+        if (username is None) or (password is None):
+            error = "Username and password are required"
+        else:
+            user = db.execute(
+                "SELECT * FROM user WHERE username=?", (username)
+            ).fetchone()
+
+            if (user is not None) and (not check_password_hash(user["password"], password)):
+                error = "Invalid credentials"
+        
+        if error is None:
+            session.clear()
+            session["user_id"] = user["id"]
+            return redirect(url_for("index"))
+
+        flash(error)
+
+    return render_template("auth/login.html")
