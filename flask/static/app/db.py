a996a9d84b1d174be40fd25873aff15f764052ce
Michael George <michael.g.george@gmail.com>
2022-10-08 15:44:02 +0530

    Create fn to init_db in static flask app

--- flask/static/app/db.py
+++ flask/static/app/db.py
@@ -19,3 +19,9 @@ def close_db():
     if db is not None:
         db.close()
 
+def init_db():
+    db = get_db()
+
+    with current_app.open_resource("schema.sql") as f:
+        db.executescript(f.read().decode("utf-8"))
+    
