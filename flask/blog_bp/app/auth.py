b68cdb346b660bba4a1c5005af58adcb2e9de827
Michael George <michael.g.george@gmail.com>
2022-10-09 09:32:10 +0530

    Add before_app_request decorator to blog_bp flask app

--- flask/blog_bp/app/auth.py
+++ flask/blog_bp/app/auth.py
@@ -60,4 +60,14 @@ def login():
         flash(error)
     
     return render_template("auth/login.html")
-    
+
+@bp.before_app_request
+def load_logged_in_user():
+    user_id = session.get("user_id", None)
+
+    if user_id == None:
+        g.user = None
+    else:
+        g.user = get_db().execute(
+            "SELECT * FROM user where id=?", (user_id,)
+        ).fetchone()
