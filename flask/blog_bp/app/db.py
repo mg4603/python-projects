1b57d7f7312291ee4a5279233239e3240118bea4
Michael George <michael.g.george@gmail.com>
2022-10-09 08:44:42 +0530

    Add fn to get_db in blog_bp flask app

---
+++ flask/blog_bp/app/db.py
@@ -0,0 +1,12 @@
+from sqlite3 import connect, Row, PARSE_DECLTYPES
+from flask import g, current_app
+from click import echo, command
+
+def get_db():
+    if "db" not in g:
+        g.db = connect(
+            current_app.config["DATABASE"],
+            detect_types=PARSE_DECLTYPES
+        )
+    
+    return g.db
