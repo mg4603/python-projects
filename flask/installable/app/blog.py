d13e5e870f04f5b784cc79ebf58fd1970099a883
Michael George <michael.g.george@gmail.com>
2022-10-09 23:11:18 +0530

    Create fn to get post in installable flask app

--- flask/installable/app/blog.py
+++ flask/installable/app/blog.py
@@ -2,6 +2,7 @@ from flask import (
     Blueprint, render_template, request, redirect, url_for, flash,
     g
 )
+from werkzeug.exceptions import abort
 from .auth import login_required
 from .db import get_db
 
@@ -42,3 +43,18 @@ def create():
         flash(error)
     return render_template('blog/create.html')
 
+def get_post(id, check_author=True):
+    post = get_db().execute(
+        'SELECT p.id, title, body, author_id, created, username'
+        ' FROM post p JOIN user u ON p.author_id=u.id'
+        ' WHERE p.id=?',
+        (id, )
+    ).fetchone()
+
+    if post is None:
+        abort(404, f'Post id {id} doesn\'t exist')
+    
+    if check_author and post['author_id'] != g.user['id']:
+        abort(403)
+    
+    return post
