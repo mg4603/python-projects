0f5e4c0638ee696026cec17282278719900e8f0e
Michael George <michael.g.george@gmail.com>
2022-10-09 16:36:17 +0530

    Add fn to get_db in installable flask app

---
+++ flask/installable/app/db.py
@@ -0,0 +1,13 @@
+from sqlite3 import Row, connect, PARSE_DECLTYPES
+from click import command, echo
+from flask import current_app, g
+
+def get_db():
+    if "db" not in g:
+        g.db = connect(
+            current_app.config["DATABASE"],
+            detect_types=PARSE_DECLTYPES
+        )
+        g.db.row_factory = Row
+    
+    return g.db
