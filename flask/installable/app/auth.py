e04d869d2482273526dec5dfa7713012413a2ab7
Michael George <michael.g.george@gmail.com>
2022-10-09 19:37:06 +0530

    Create auth.login route in installable flask app

--- flask/installable/app/auth.py
+++ flask/installable/app/auth.py
@@ -33,3 +33,28 @@ def register():
 
     return render_template("auth/register.html")
 
+@bp.route("/login", methods=("GET", "POST"))
+def login():
+    if request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
+        db = get_db()
+        error = None
+
+        if (username is None) or (password is None):
+            error = "Username and password are required"
+        else:
+            user = db.execute(
+                "SELECT * FROM user WHERE username = ?", (username,)
+            ).fetchone()
+            if (user is None ) or (not check_password_hash(user["password"], password)):
+                error = "Invalid credentials"
+            
+            if error is None:
+                session.clear()
+                session["user_id"] = user["id"]
+                return redirect(url_for("index"))
+
+        flash(error)
+
+    return render_template("auth/login.html")
