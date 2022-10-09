39970010f44461c870cbaceb250e961ffb3cef14
Michael George <michael.g.george@gmail.com>
2022-10-09 09:21:46 +0530

    Create register route for blog_bp flask app

--- flask/blog_bp/app/auth.py
+++ flask/blog_bp/app/auth.py
@@ -1,6 +1,35 @@
 from flask import (
-    Blueprint, g, current_app, session, url_for, redirect
+    Blueprint, g, current_app, session, url_for, redirect, request,
+    flash, render_template
 )
 from .db import get_db
+from werkzeug.security import generate_password_hash, check_password_hash
 
 bp = Blueprint("auth", __name__, url_prefix="/auth")
+
+@bp.route("/register", methods=("GET", "POST"))
+def register():
+    if request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
+        db = get_db()
+        error = None
+
+        if (username is None) or (password is None):
+            error = "Username and password are required"
+
+        if error is None:
+            try:
+                db.execute(
+                    "INSERT INTO user (username, password) VALUES (?, ?)",
+                    (username, generate_password_hash(password)),
+                )
+                db.commit()
+            except db.IntegrityError:
+                error = f"Username {username} already exists."
+            else:
+                return redirect(url_for('auth.login'))
+
+        flash(error)
+    
+    return render_template("auth/register.html")
