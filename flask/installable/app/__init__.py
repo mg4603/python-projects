dfa54db9ba5f636ca999f0f3d7f7359aa8ff82fc
Michael George <michael.g.george@gmail.com>
2022-10-09 21:24:04 +0530

    Register blog_bp with app in installable flask app

--- flask/installable/app/__init__.py
+++ flask/installable/app/__init__.py
@@ -25,4 +25,8 @@ def create_app(test_config=None):
     from .auth import bp as auth_bp
     app.register_blueprint(auth_bp)
 
+    from .blog import bp as blog_bp
+    app.register_blueprint(blog_bp)
+    app.add_url_rule("/", endpoint="index")
+    
     return app
