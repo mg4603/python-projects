a96bc727fad5b7f11708a7c4217ce96d3a8cd8a4
Michael George <michael.g.george@gmail.com>
2022-10-10 14:26:13 +0530

    Create blog.index route in tests flask app

--- flask/tests/app/blog.py
+++ flask/tests/app/blog.py
@@ -1,6 +1,17 @@
 from flask import (
-    Blueprint
+    Blueprint, render_template
 )
 from .db import get_db
 
 bp = Blueprint('blog', __name__)
+
+@bp.route('/')
+def index():
+    db = get_db()
+    posts = db.execute(
+        'SELECT p.id, title, body, created, username, author_id'
+        ' FROM post p JOIN user u on p.author_id = u.id'
+        ' ORDER BY created DESC'
+    ).fetchall()
+    return render_template('blog/index.html', posts=posts)
+
