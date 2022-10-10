dc96dcc64531d28d9fdf60b535f91f20a8b566c0
Michael George <michael.g.george@gmail.com>
2022-10-10 20:17:11 +0530

    mend

--- flask/tests/tests/conftest.py
+++ flask/tests/tests/conftest.py
@@ -1,9 +1,11 @@
 from os.path import join, dirname
 from app import create_app
 from app.db import init_db, get_db
+from pytest import fixture
+from tempfile import mkstemp
 
-def get_data_sql():
-    with open(join(dirname(__file__), 'data.sql'), 'rb') as f:
+def get_data_sql(filename):
+    with open(join(dirname(__file__), filename), 'rb') as f:
         _data_sql = f.read().decode('utf-8')
     
     return _data_sql
