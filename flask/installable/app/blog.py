5647098602608ff556186ca4bfe77ced2c80728b
Michael George <michael.g.george@gmail.com>
2022-10-10 10:16:44 +0530

    Fix error in blog.update route in installable app
    Removed space in blog.update route url.

--- flask/installable/app/blog.py
+++ flask/installable/app/blog.py
@@ -59,7 +59,7 @@ def get_post(id, check_author=True):
     
     return post
 
-@bp.route('/<int: id>/update', methods=['GET', 'POST'])
+@bp.route('/<int:id>/update', methods=['GET', 'POST'])
 def update(id):
     post = get_post(id)
 
@@ -82,5 +82,5 @@ def update(id):
             )
             db.commit()
             return redirect(url_for('blog.index'))
-            
+
     return render_template("blog/update.html", post=post)
