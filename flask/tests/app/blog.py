4e0cd7ffd00d80dfe213d48ef9f395b1df0c8e50
Michael George <michael.g.george@gmail.com>
2022-10-10 14:48:20 +0530

    Create blog.create route in tests flask app

--- flask/tests/app/blog.py
+++ flask/tests/app/blog.py
@@ -1,6 +1,8 @@
 from flask import (
-    Blueprint, render_template
+    Blueprint, render_template, request, flash, url_for,
+    redirect, g
 )
+from .auth import login_required
 from .db import get_db
 
 bp = Blueprint('blog', __name__)
@@ -15,3 +17,27 @@ def index():
     ).fetchall()
     return render_template('blog/index.html', posts=posts)
 
+@bp.route('/create/', methods=('GET', 'POST'))
+@login_required
+def create():
+    if request.method == 'POST':
+        title = request.form['title']
+        body = request.form['body']
+        error = None
+
+        if title is None:
+            error = 'Title is required'
+
+        if error is not None:
+            flash(error)
+        else:
+            db = get_db()
+            db.execute(
+                'INSERT INTO post(author_id, title, body)'
+                ' VALUES (?, ?, ?)',
+                (g.user['id'], title, body)
+            )
+            db.commit()
+            return redirect(url_for('blog.index'))
+                
+    return render_template('blog/create.html')
