6434d7873993e53013598089378e4c97b8d02e37
Michael George <michael.g.george@gmail.com>
2022-10-10 12:03:15 +0530

    Create auth_bp.login route in tests flask app

--- flask/tests/app/auth.py
+++ flask/tests/app/auth.py
@@ -1,5 +1,6 @@
 from flask import (
-    Blueprint, render_template, request, flash, redirect, url_for
+    Blueprint, render_template, request, flash, redirect, url_for,
+    session
 )
 from werkzeug.security import generate_password_hash, check_password_hash
 from .db import get_db
@@ -31,3 +32,29 @@ def register():
                 return redirect(url_for('auth.login'))
         flash(error)
     return render_template('auth/register.html')
+
+@bp.route('/login', methods=('GET', 'POST'))
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        error = None
+        db = get_db()
+
+        if (username is None) or (password is None):
+            error = 'Username and password are required'
+        else:
+            user = db.execute(
+                'SELECT * FROM user WHERE username=?', (username,)
+            ).fetchone()
+
+            if (user is None) or (not check_password_hash(user['password'], password)):
+                error = 'Invalid credentials'
+            else:
+                session.clear()
+                session['user_id'] = user['id']
+                return redirect(url_for('index')) 
+
+        flash(error)
+        
+    return render_template('auth/login.html')
