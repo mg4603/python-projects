0ad87319a8fb68b08a576fb6837d195dd748e90a
Michael George <michael.g.george@gmail.com>
2022-10-10 10:33:55 +0530

    Add config to setup in installable flask app

--- flask/installable/setup.py
+++ flask/installable/setup.py
@@ -3,6 +3,13 @@ try:
 except ImportError:
     from distutils.core import find_packages , setup
 
+config = {
+    'name': 'Installable flask app',
+    'version': '0.0.1',
+    'packages': find_packages(),
+    'include_package_data': True,
+    'install_requires': ['flask', ],
+}   
 
 if __name__ == '__main__':
     setup(**config)
