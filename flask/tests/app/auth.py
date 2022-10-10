178f8c98c1a8a28b943d8bb3423e9c7a5f8516de
Michael George <michael.g.george@gmail.com>
2022-10-10 11:57:09 +0530

    Create register route in auth_bp in tests flask app

--- flask/tests/app/auth.py
+++ flask/tests/app/auth.py
@@ -1,6 +1,33 @@
 from flask import (
-    Blueprint
+    Blueprint, render_template, request, flash, redirect, url_for
 )
+from werkzeug.security import generate_password_hash, check_password_hash
 from .db import get_db
 
 bp = Blueprint('auth', __name__, url_prefix='/auth')
+
+@bp.route('/register', methods=('GET', 'POST'))
+def register():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        error = None
+        db = get_db()
+
+        if (not username) or (not password):
+            error = 'Username and password required'
+
+        if error is None:
+            try:
+                db.execute(
+                    'INSERT INTO user(username, password)'
+                    ' VALUES (?, ?)',
+                    (username, generate_password_hash(password)),
+                )
+                db.commit()
+            except db.IntegrityError:
+                error = f'Username {username} already exists.'
+            else:
+                return redirect(url_for('auth.login'))
+        flash(error)
+    return render_template('auth/register.html')
