bde85abb2f1b0dd3e05032ac6eda335829ec639c
Michael George <michael.g.george@gmail.com>
2022-10-10 15:35:08 +0530

    Add config info to setup tests flask app

--- flask/tests/setup.py
+++ flask/tests/setup.py
@@ -3,6 +3,14 @@ try:
 except ImportError:
     from distutils.core import find_packages, setup
 
+config = {
+    'name': 'Tests',
+    'version': '0.0.1',
+    'packages': find_packages(),
+    'include_package_data': True,
+    'install_requires': ['flask',
+    ],
+}
 
 if __name__ == '__main__':
     setup(**config)
