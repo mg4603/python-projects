0b9ad1e451d3f4aa8926ecb91fb4defa7d302c2c
Michael George <michael.g.george@gmail.com>
2022-10-10 12:24:59 +0530

    Add login required decorator to tests flask app

--- flask/tests/app/auth.py
+++ flask/tests/app/auth.py
@@ -4,6 +4,7 @@ from flask import (
 )
 from werkzeug.security import generate_password_hash, check_password_hash
 from .db import get_db
+from functools import wraps
 
 bp = Blueprint('auth', __name__, url_prefix='/auth')
 
@@ -75,3 +76,13 @@ def load_logged_in_user():
 def logout():
     session.clear()
     return redirect(url_for('index'))
+
+def login_required(view):
+    @wraps(view)
+    def wrapped_view(**kwargs):
+        if g.user is None:
+            return redirect(url_for('auth.login'))
+
+        return view(**kwargs)
+        
+    return wrapped_view
