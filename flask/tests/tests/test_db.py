7ac96b8f54105e234c4be827c2f777a3411b8ffa
Michael George <michael.g.george@gmail.com>
2022-10-11 10:16:30 +0530

    Complete test_init_db_command fn in tests flask app

--- flask/tests/tests/test_db.py
+++ flask/tests/tests/test_db.py
@@ -1,6 +1,7 @@
 from sqlite3 import ProgrammingError
 from app.db import get_db
 from pytest import raises
+from monkeypatch import setattr
 
 def test_get_close_db(app):
     with app.app_context():
@@ -19,4 +20,7 @@ def test_init_db_command(runner, monkeypatch):
     def fake_init_db():
         Recorder.called = True
     
-    
+    setattr('app.db.init_db', fake_init_db)
+    result = runner.invoke(args=['init-db'])
+    assert 'Initialized' in result.output
+    assert Recorder.called
