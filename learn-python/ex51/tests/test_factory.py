24b18c77205f0f61774670d61b58ac4b3b336b0a
Michael George <michael.g.george@gmail.com>
2022-10-11 20:12:07 +0530

    Remove test for hello.index from test_factory in ex51

--- learn-python/ex51/tests/test_factory.py
+++ learn-python/ex51/tests/test_factory.py
@@ -3,11 +3,3 @@ from app import create_app
 def test_config():
     assert not create_app().testing
     assert create_app({'TESTING': True}).testing
-
-def test_hello(client):
-    response = client.post(
-        '/hello/form/',
-        data={'greet': '', 'name': ''
-    })
-    assert response.headers['Location'] == '/hello/q%3Fgreet%3DHello%26name%3DNobody'
-    
