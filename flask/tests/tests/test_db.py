1bd431c7ff8d7c700534c249ebe98ee1920c29c2
Michael George <michael.g.george@gmail.com>
2022-10-11 10:44:35 +0530

    Fix error in test_db in tests flask app
    Explicitly imported monkeypatch to fix dependency errors pytest
    monkeypatch.

--- flask/tests/tests/test_db.py
+++ flask/tests/tests/test_db.py
@@ -1,7 +1,7 @@
 from sqlite3 import ProgrammingError
 from app.db import get_db
 from pytest import raises
-from monkeypatch import setattr
+from _pytest.monkeypatch import MonkeyPatch
 
 def test_get_close_db(app):
     with app.app_context():
@@ -13,14 +13,14 @@ def test_get_close_db(app):
     
     assert 'closed' in str(e.value)
 
-def test_init_db_command(runner, monkeypatch):
+def test_init_db_command(runner, monkeypatch: MonkeyPatch):
     class Recorder(object):
         called = False
     
     def fake_init_db():
         Recorder.called = True
     
-    setattr('app.db.init_db', fake_init_db)
+    monkeypatch.setattr('app.db.init_db', fake_init_db)
     result = runner.invoke(args=['init-db'])
-    assert 'Initialized' in result.output
+    assert 'Initialize' in result.output
     assert Recorder.called
