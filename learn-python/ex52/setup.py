04de0005d0f495845b19dbc5b86ca8a4e34116a3
Michael George <michael.g.george@gmail.com>
2022-10-12 11:47:22 +0530

    Add config to ex52 setup

--- learn-python/ex52/setup.py
+++ learn-python/ex52/setup.py
@@ -3,7 +3,17 @@ try:
 except ImportError:
     from distutils.core import find_packages, setup
 
-config = {}
+config = {
+    'name': 'ex52',
+    'version': '0.0.1',
+    'description': 'Ex43 game refactored to use a web ui',
+    'package': find_packages(),
+    'include_package_data': True,
+    'install_requires': [
+        'flask',
+    ],
+    'author': 'mg4603',
+}
 
 if __name__ == '__main__':
     setup(**config)
