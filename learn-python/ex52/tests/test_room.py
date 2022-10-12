6530ca1e8302cfbb669689d6bb9e308016f8b188
Michael George <michael.g.george@gmail.com>
2022-10-12 13:05:06 +0530

    Test go and add_path methods in Room class in ex52

--- learn-python/ex52/tests/test_room.py
+++ learn-python/ex52/tests/test_room.py
@@ -7,3 +7,13 @@ def test_room():
     assert gold.name == "GoldRoom"
     assert gold.paths == {}
 
+def test_room_go_add_paths():
+    center = Room("Center", "Test room in the center.")
+    north = Room("North", "Test room in the north.")
+    south = Room("South", "Test room in the south.")
+
+    center.add_paths({"north": north,
+                    "south": south})
+    
+    assert center.go("north") == north
+    assert center.go("south") == south
