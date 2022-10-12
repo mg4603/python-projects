10b54f3e83880974961b9f312a253099a9c75c79
Michael George <michael.g.george@gmail.com>
2022-10-12 12:12:07 +0530

    Create app factory for ex52

---
+++ learn-python/ex52/app/__init__.py
@@ -0,0 +1,20 @@
+from flask import Flask
+from os import makedirs
+
+def create_app(test_config=None):
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_mapping(
+        SECRET_KEY='dev',
+    )
+
+    if test_config is None:
+        app.config.from_pyfile('config.py', silent=True)
+    else:
+        app.config.from_mapping(test_config)
+    
+    try:
+        makedirs(app.instance_path)
+    except OSError:
+        pass
+
+    return app
