e150dcf0b2c4af1b2e4af452ddd55eb52c62135d
Michael George <michael.g.george@gmail.com>
2022-10-15 00:08:23 +0530

    Add config to setup in session flask app

--- flask/session/setup.py
+++ flask/session/setup.py
@@ -3,5 +3,16 @@ try:
 except ImportError:
     from distutils.core import setup, find_packages
 
+config = {
+    'name': 'Session Flask app',
+    'version': '0.0.1',
+    'packages': find_packages(),
+    'include_package_data': True,
+    'install_requires': [
+        'flask',
+    ],
+    'description': 'Flask app to implement session'
+}
+
 if __name__ == "__main__":
     setup(**config)
