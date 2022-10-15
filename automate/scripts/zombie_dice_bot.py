685354de8fa9d437dbb875414e7fd1374aa5f7d1
Michael George <michael.g.george@gmail.com>
2022-10-15 18:13:46 +0530

    Add runner code to zombie_dice_bot in automate

--- automate/scripts/zombie_dice_bot.py
+++ automate/scripts/zombie_dice_bot.py
@@ -14,3 +14,15 @@ class MyZombie:
             else:
                 break
 
+zombies = (
+    zombiedice.examples.RandomCoinFlipZombie(name='Random'),
+    zombiedice.examples.RollsUntilInTheLeadZombie(name='Until Leading'),
+    zombiedice.examples.MinNumShotgunsThenStopsZombie(name='Stop at 2\
+    Shotguns', minShotguns=2),
+    zombiedice.examples.MinNumShotgunsThenStopsZombie(name='Stop at 1\
+    Shotgun', minShotguns=1),
+    MyZombie(name='My Zombie Bot'),
+)
+
+# zombiedice.runTournament(zombies=zombies, numGames=1000)
+zombiedice.runWebGui(zombies=zombies, numGames=1000)
