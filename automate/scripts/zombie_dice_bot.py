c729316dc76514a3c59f74cc0276a50d612b922e
Michael George <michael.g.george@gmail.com>
2022-10-15 18:13:27 +0530

    Define turn for MyZombie in zombie_dice_bot,automate

--- automate/scripts/zombie_dice_bot.py
+++ automate/scripts/zombie_dice_bot.py
@@ -4,3 +4,13 @@ class MyZombie:
     def __init__(self, name):
         self.name = name
     
+    def turn(self, game_state):
+        dice_roll_results = zombiedice.roll()
+        brains = 0
+        while dice_roll_results is not None:
+            brains += dice_roll_results['brains']
+            if brains < 2:
+                dice_roll_results = zombiedice.roll()
+            else:
+                break
+
