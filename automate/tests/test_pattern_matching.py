7430dc36af0010559b59190704144bb337f1bb90
Michael George <michael.g.george@gmail.com>
2022-10-21 14:51:40 +0530

    Test findPhNumber in test_pattern_matching

--- automate/tests/test_pattern_matching.py
+++ automate/tests/test_pattern_matching.py
@@ -1,4 +1,4 @@
-from scripts.pattern_matching import isPhoneNumber
+from scripts.pattern_matching import findPhNumber, isPhoneNumber
 
 
 def test_isPhoneNumber():
@@ -6,3 +6,7 @@ def test_isPhoneNumber():
     assert isPhoneNumber('01234-567891')
     assert not isPhoneNumber('0123456789')
     assert not isPhoneNumber('0123-123121')
+
+def test_findPhNumber():
+    assert findPhNumber('My number is 01234-567891') == 'Phone number found: 01234-567891'
+    assert findPhNumber('No phone number here') == 'No phone number found in text'
