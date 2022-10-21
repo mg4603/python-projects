5092c184c74ddd7f936b0b98b066e16491f180f0
Michael George <michael.g.george@gmail.com>
2022-10-21 14:45:15 +0530

    Fix logic error in isPhoneNumber in pattern_matching

--- automate/scripts/pattern_matching.py
+++ automate/scripts/pattern_matching.py
@@ -4,18 +4,18 @@ def isPhoneNumber(string):
     if len(string) != 11 and len(string) != 12:
         return False
 
-    for i in range(0, 6):
+    for i in range(0, 5):
         if not string[i].isdecimal():
             return False
     
     if len(string) == 12:
-        if string[6] != ' ' and string[6] != '-':
+        if string[5] != ' ' and string[5] != '-':
             return False
-        for i in range(7, 12):
+        for i in range(6, 12):
             if not string[i].isdecimal():
                 return False
     else:
-        for i in range(6, 11):
+        for i in range(5, 11):
             if not string[i].isdecimal():
                 return False
     
