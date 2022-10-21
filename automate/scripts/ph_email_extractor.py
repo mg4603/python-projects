ef10d5694353536f52323169b8c11c3d1cff4240
Michael George <michael.g.george@gmail.com>
2022-10-21 22:58:05 +0530

    Add fn extractor to ph_email_extractor

--- automate/scripts/ph_email_extractor.py
+++ automate/scripts/ph_email_extractor.py
@@ -9,8 +9,39 @@
 4) Copy back to the clipboard
 5) Display a message if nothing was found
 """
-
 from pyperclip import copy, paste
+from re import VERBOSE, compile
+
+def extractor(text):
+    email_regex = compile(
+        r'''
+        ([a-zA-Z0-9._%+-]+)       # username
+        (@)                       # @ symbol
+        ([a-zA-Z0-9-]+)          # domain name
+        (\.[a-zA-Z]{2,4})      # dot something
+        #(\.[a-zA-Z]{2, 4})?     # tld
+        ''',
+        VERBOSE
+    ) 
+
+    phone_number_regex = compile(
+        r'''
+        (\+\d{1,3})?        # country code
+        [-\ ]?              # separator
+        (\d{2,3})?        # region code
+        [-\ ]?              # separator
+        (\d{4})             # first five digits
+        [-\ ]?              # separator
+        (\d{4})
+        ''',
+        VERBOSE
+    )
+    emails = [''.join(email) for email in  email_regex.findall(text)]
+    phone_numbers = ['-'.join(list(phone_number_groups)) for phone_number_groups in phone_number_regex.findall(text)]
+    return 'Phone Numbers: \n\t{}\nEmails: \n\t{}'.format(
+        '\n\t'.join(phone_numbers),
+        '\n\t'.join(emails)
+    )
 
 if __name__ == "__main__":
     text = paste()
