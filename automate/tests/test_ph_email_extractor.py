771a81962f61eae4abffc879b57b284249d3daa4
Michael George <michael.g.george@gmail.com>
2022-10-22 06:00:28 +0530

    Test email_extractor in automate/tests

--- automate/tests/test_ph_email_extractor.py
+++ automate/tests/test_ph_email_extractor.py
@@ -4,4 +4,7 @@ def test_ph_number_extractor():
     assert ph_number_extractor('2242081524') == ['22-4208-1524']
     assert ph_number_extractor('42081524') == ['4208-1524']
     assert ph_number_extractor('22-4208-1524') == ['22-4208-1524']
-    
+
+def test_email_extractor():
+    assert email_extractor(('lol@lol.lol.edu')) == ['lol@lol.lol.edu']
+    assert email_extractor('asf lol.lol@lol.gov.us asdf') == ['lol.lol@lol.gov.us']
