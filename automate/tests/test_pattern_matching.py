65a5e5906ad11d4e1b1860f4fc83a6921e936ad0
Michael George <michael.g.george@gmail.com>
2022-10-22 09:09:53 +0530

    Test q20 in test_pattern_matching

--- automate/tests/test_pattern_matching.py
+++ automate/tests/test_pattern_matching.py
@@ -1,4 +1,4 @@
-from scripts.pattern_matching import findPhNumber, isPhoneNumber
+from scripts.pattern_matching import findPhNumber, isPhoneNumber, q20
 
 
 def test_isPhoneNumber():
@@ -10,3 +10,10 @@ def test_isPhoneNumber():
 def test_findPhNumber():
     assert findPhNumber('My number is 01234-567891') == 'Phone number found: 01234-567891'
     assert findPhNumber('No phone number here') == 'No phone number found in text'
+
+def test_q20():
+    assert q20('42')
+    assert q20('1,234')
+    assert q20('6,368,745')
+    assert not q20('12,34,567')
+    assert not q20('1234')
