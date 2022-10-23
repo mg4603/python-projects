87e030f5580753cb69fab47c734d8293b7c72b5a
Michael George <michael.g.george@gmail.com>
2022-10-23 17:11:18 +0530

    Fix leap year regex in date_detection

--- automate/scripts/date_detection.py
+++ automate/scripts/date_detection.py
@@ -29,7 +29,7 @@ def check_31_days(date):
 
 def check_leap_feb(date):
     leap_feb_regex = compile(
-        r'(0[1-9]|1[0-9]|2[0-8])'
+        r'(0[1-9]|[12][0-9])'
     )
     return leap_feb_regex.search(date)
 
