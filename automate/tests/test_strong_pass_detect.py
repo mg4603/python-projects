55a5b7d187bc682571645f8aecfa87c42728b1c1
Michael George <michael.g.george@gmail.com>
2022-10-24 19:18:04 +0530

    Test check_upper in test_strong_pass_detect, automate

--- automate/tests/test_strong_pass_detect.py
+++ automate/tests/test_strong_pass_detect.py
@@ -1,4 +1,6 @@
-from scripts.strong_pass_detect import check_length
+from scripts.strong_pass_detect import (
+    check_length, check_upper
+) 
 
 def test_check_length():
     assert check_length('asdfghjl')
@@ -6,3 +8,10 @@ def test_check_length():
     assert check_length('asd@1 jk')
     assert not check_length('asdf')
     assert not check_length('asdf123')
+
+def test_check_upper():
+    assert check_upper('Aasdf')
+    assert check_upper('asfAasdf')
+    assert check_upper('  A  ')
+    assert not check_upper('asfd')
+    assert not check_upper('1234')
