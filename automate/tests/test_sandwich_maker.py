be4585590f20ede85a4ad35b1c362229deb440fd
Michael George <michael.g.george@gmail.com>
2022-10-25 20:36:55 +0530

    Test choose_protein in test_sandwich_maker, automate

--- automate/tests/test_sandwich_maker.py
+++ automate/tests/test_sandwich_maker.py
@@ -1,5 +1,6 @@
 from scripts.sandwichMaker import (
-    choose_bread
+    choose_bread,
+    choose_protein
 )
 from pytest import raises
 
@@ -9,3 +10,11 @@ def test_choose_bread():
     assert choose_bread('sourdough') == 2.75
     with raises(Exception):
         choose_bread('asdf')
+
+def test_choose_protein():
+    assert choose_protein('chicken') == 2
+    assert choose_protein('turkey') == 2.35
+    assert choose_protein('ham') == 2.5
+    assert choose_protein('tofu') == 3.5
+    with raises(Exception):
+        choose_protein('asf')
