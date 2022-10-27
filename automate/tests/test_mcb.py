96d517b64c9e9147abb9c64ae0d2d71da5ff35b7
Michael George <michael.g.george@gmail.com>
2022-10-28 01:00:07 +0530

    Test clear_shelf in test_mcb in automate

--- automate/tests/test_mcb.py
+++ automate/tests/test_mcb.py
@@ -1,5 +1,5 @@
 from scripts.mcb import (
- delete_from_shelf, save_shelf, load_shelf   
+ clear_shelf, delete_from_shelf, save_shelf, load_shelf   
 )
 
 def test_shelf():
@@ -11,3 +11,7 @@ def test_delete_from_shelf():
     assert load_shelf() == {'new': 'first clip', 'new2': 'second clip'}
     delete_from_shelf('new2')
     assert load_shelf() == {'new': 'first clip'}
+
+def test_clear_shelf():
+    clear_shelf()
+    assert load_shelf() == {}
