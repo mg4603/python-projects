be2d417cc30720fbbbfcb057233d3bbee99fa6b5
Michael George <michael.g.george@gmail.com>
2022-10-28 00:56:02 +0530

    Test delete_from_shelf in test_mcb, automate

--- automate/tests/test_mcb.py
+++ automate/tests/test_mcb.py
@@ -1,7 +1,13 @@
 from scripts.mcb import (
- save_shelf, load_shelf   
+ delete_from_shelf, save_shelf, load_shelf   
 )
 
 def test_shelf():
     save_shelf('new', 'first clip')
     assert load_shelf() == {'new':'first clip'}
+
+def test_delete_from_shelf():
+    save_shelf('new2', 'second clip')
+    assert load_shelf() == {'new': 'first clip', 'new2': 'second clip'}
+    delete_from_shelf('new2')
+    assert load_shelf() == {'new': 'first clip'}
