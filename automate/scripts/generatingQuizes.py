4a0eb474cfaa12f123790f975ce21953faa57e03
Michael George <michael.g.george@gmail.com>
2022-10-27 18:02:39 +0530

    Add main fn to generatingQuizes

--- automate/scripts/generatingQuizes.py
+++ automate/scripts/generatingQuizes.py
@@ -1,58 +1,4 @@
-# Store the states and their capitals in a dictionary
-capitals = {
 
-    'Alabama': 'Montgomery', 
-    'Alaska': 'Juneau', 
-    'Arizona': 'Phoenix',
-    'Arkansas': 'Little Rock', 
-    'California': 'Sacramento', 
-    'Colorado': 'Denver',
-    'Connecticut': 'Hartford', 
-    'Delaware': 'Dover', 
-    'Florida': 'Tallahassee',
-    'Georgia': 'Atlanta', 
-    'Hawaii': 'Honolulu', 
-    'Idaho': 'Boise', 
-    'Illinois': 'Springfield', 
-    'Indiana': 'Indianapolis', 
-    'Iowa': 'Des Moines', 
-    'Kansas': 'Topeka', 
-    'Kentucky': 'Frankfort', 
-    'Louisiana': 'Baton Rouge', 
-    'Maine': 'Augusta', 
-    'Maryland': 'Annapolis', 
-    'Massachusetts': 'Boston', 
-    'Michigan': 'Lansing', 
-    'Minnesota': 'Saint Paul', 
-    'Mississippi': 'Jackson', 
-    'Missouri': 'Jefferson City', 
-    'Montana': 'Helena', 
-    'Nebraska': 'Lincoln', 
-    'Nevada': 'Carson City', 
-    'New Hampshire': 'Concord', 
-    'New Jersey': 'Trenton', 
-    'New Mexico': 'Santa Fe', 
-    'New York': 'Albany', 
-    'North Carolina': 'Raleigh',
-    'North Dakota': 'Bismarck', 
-    'Ohio': 'Columbus', 
-    'Oklahoma': 'Oklahoma City',
-    'Oregon': 'Salem', 
-    'Pennsylvania': 'Harrisburg', 
-    'Rhode Island': 'Providence',
-    'South Carolina': 'Columbia', 
-    'South Dakota': 'Pierre', 
-    'Tennessee': 'Nashville', 
-    'Texas': 'Austin', 
-    'Utah': 'Salt Lake City', 
-    'Vermont': 'Montpelier', 
-    'Virginia': 'Richmond', 
-    'Washington': 'Olympia', 
-    'West Virginia': 'Charleston', 
-    'Wisconsin': 'Madison', 
-    'Wyoming': 'Cheyenne'
-
-}
 
 from random import shuffle, sample
 from pathlib import Path
@@ -129,3 +75,62 @@ def create_all_quizzes(numOfQuizzes, capitalsDict):
         quizString, answerString = create_quiz(quizNum, capitalsDict)
         write_quiz_to_file(quizNum, quizString, path)
         write_answer_key_to_file(quizNum, answerString, path)
+
+
+def main():
+    # Store the states and their capitals in a dictionary
+    capitals = {
+
+        'Alabama': 'Montgomery', 
+        'Alaska': 'Juneau', 
+        'Arizona': 'Phoenix',
+        'Arkansas': 'Little Rock', 
+        'California': 'Sacramento', 
+        'Colorado': 'Denver',
+        'Connecticut': 'Hartford', 
+        'Delaware': 'Dover', 
+        'Florida': 'Tallahassee',
+        'Georgia': 'Atlanta', 
+        'Hawaii': 'Honolulu', 
+        'Idaho': 'Boise', 
+        'Illinois': 'Springfield', 
+        'Indiana': 'Indianapolis', 
+        'Iowa': 'Des Moines', 
+        'Kansas': 'Topeka', 
+        'Kentucky': 'Frankfort', 
+        'Louisiana': 'Baton Rouge', 
+        'Maine': 'Augusta', 
+        'Maryland': 'Annapolis', 
+        'Massachusetts': 'Boston', 
+        'Michigan': 'Lansing', 
+        'Minnesota': 'Saint Paul', 
+        'Mississippi': 'Jackson', 
+        'Missouri': 'Jefferson City', 
+        'Montana': 'Helena', 
+        'Nebraska': 'Lincoln', 
+        'Nevada': 'Carson City', 
+        'New Hampshire': 'Concord', 
+        'New Jersey': 'Trenton', 
+        'New Mexico': 'Santa Fe', 
+        'New York': 'Albany', 
+        'North Carolina': 'Raleigh',
+        'North Dakota': 'Bismarck', 
+        'Ohio': 'Columbus', 
+        'Oklahoma': 'Oklahoma City',
+        'Oregon': 'Salem', 
+        'Pennsylvania': 'Harrisburg', 
+        'Rhode Island': 'Providence',
+        'South Carolina': 'Columbia', 
+        'South Dakota': 'Pierre', 
+        'Tennessee': 'Nashville', 
+        'Texas': 'Austin', 
+        'Utah': 'Salt Lake City', 
+        'Vermont': 'Montpelier', 
+        'Virginia': 'Richmond', 
+        'Washington': 'Olympia', 
+        'West Virginia': 'Charleston', 
+        'Wisconsin': 'Madison', 
+        'Wyoming': 'Cheyenne'
+
+    }
+    create_all_quizzes(35, capitals)
