126d2720c6fddfcb65ac92a66cf210553180f1f5
Michael George <michael.g.george@gmail.com>
2022-10-28 19:56:56 +0530

    Fix errors in parse_args in test_mad_libs, automate

--- automate/scripts/mad_libs.py
+++ automate/scripts/mad_libs.py
@@ -1,16 +1,16 @@
-from sys import argv, exit
+import sys
 import pyinputplus as pyip
 
 def parse_args():
-    if len(argv) == 3:
-        in_file = argv[1]
-        out_file = argv[2]
+    if len(sys.argv) == 3:
+        in_file = sys.argv[1]
+        out_file = sys.argv[2]
         return {'in': in_file, 'out': out_file}
-    elif len(argv) == 2:
-        in_file = argv[1]
-        return {'in', in_file}
+    elif len(sys.argv) == 2:
+        in_file = sys.argv[1]
+        return {'in': in_file}
     else:
-        exit("Enter an option\n\t1. <input_file>\n\t2. <input_file> <output_file>\n")
+        sys.exit("Enter an option\n\t1. <input_file>\n\t2. <input_file> <output_file>\n")
 
 def get_input(type):
     extra = ''
