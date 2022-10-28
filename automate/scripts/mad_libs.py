4b8a9e66a1fc22379df4d11790c74232e7c3783d
Michael George <michael.g.george@gmail.com>
2022-10-28 17:21:47 +0530

    Complete fn to parse_args in mad_libs

--- automate/scripts/mad_libs.py
+++ automate/scripts/mad_libs.py
@@ -1,7 +1,15 @@
 from sys import argv, exit
 
 def parse_args():
-    pass
+    if len(argv) == 3:
+        in_file = argv[1]
+        out_file = argv[2]
+        return {'in': in_file, 'out': out_file}
+    elif len(argv) == 2:
+        in_file = argv[1]
+        return {'in', in_file}
+    else:
+        exit("Enter an option\n\t1. <input_file>\n\t2. <input_file> <output_file>\n")
 
 def parse_text(text):
     pass
