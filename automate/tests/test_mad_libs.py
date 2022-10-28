8eab78b6b85672d669f5862f69e39f728878b116
Michael George <michael.g.george@gmail.com>
2022-10-28 19:56:35 +0530

    Test parse_args in mad_libs, automate

--- automate/tests/test_mad_libs.py
+++ automate/tests/test_mad_libs.py
@@ -1,9 +1,27 @@
-from scripts.mad_libs import get_input
+from scripts.mad_libs import get_input, parse_args
 import pyinputplus
+import sys
+from pytest import raises
 
 def test_get_input(monkeypatch):
     with monkeypatch.context() as m:
         m.setattr('pyinputplus.inputStr', lambda prompt='Enter an adjective': 'asdf')
         result = get_input('adjective')
     assert result == 'asdf'
-    
+
+def test_parse_args(monkeypatch):
+    with monkeypatch.context() as m:
+        m.setattr('sys.argv', [])
+        with raises(SystemExit) as pytest_wrapped_error:
+            parse_args()
+        assert pytest_wrapped_error.type == SystemExit
+
+    with monkeypatch.context() as m:
+        m.setattr('sys.argv', ['../scripts/mad_libs.py', 'in_path', 'out_path'])
+        path_dict = parse_args()
+    assert path_dict == {'in': 'in_path', 'out': 'out_path'}
+
+    with monkeypatch.context() as m:
+        m.setattr('sys.argv', ['../scripts/mad_libs.py', 'in_path'])
+        path_dict = parse_args()
+    assert path_dict == {'in': 'in_path'}
