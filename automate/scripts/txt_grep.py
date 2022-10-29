f34d15cda2445c58070de9cd089883956a03f258
Michael George <michael.g.george@gmail.com>
2022-10-29 20:21:36 +0530

    Add parse_args to txt_grep, automate

--- automate/scripts/txt_grep.py
+++ automate/scripts/txt_grep.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+import sys
 
 def get_filenames(path):
     return [filename for filename in path.rglob('*.txt')]
@@ -12,4 +13,11 @@ def check_line(regex, line):
     if regex.search(line):
         return line
     return None
+
+def parse_args():
+    if len(sys.argv) == 3:
+        return {'path': sys.argv[1], 'regex': sys.argv[2]}    
+    exit(
+        'usage: python3 txt_grep.py [path] [regex]\npath: path in which to check for .txt files'
+    )
 
