f4b849f5bc8ea21235c16378cc54477576d30cdd
Michael George <michael.g.george@gmail.com>
2022-10-29 21:25:22 +0530

    Add main fn to txt_grep, automate

--- automate/scripts/txt_grep.py
+++ automate/scripts/txt_grep.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 import sys
+from re import compile
 
 def get_filenames(path):
     return [filename for filename in path.rglob('*.txt')]
@@ -21,3 +22,17 @@ def parse_args():
         'usage: python3 txt_grep.py [path] [regex]\npath: path in which to check for .txt files'
     )
 
+def main():
+    args = parse_args()
+    if Path(args['path']).exists():
+        dir_path = Path(sys.argv[1])
+
+    filenames = get_filenames(dir_path)
+    result = []
+    for filename in filenames:
+        lines = read_file(Path(filename))
+        for line in lines:
+            regex = compile(
+                args['regex']
+            )
+            result.append(check_line(regex, line))
