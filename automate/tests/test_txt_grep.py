bae00e330d56842f3b9fb3e2b85ae8521409c4dc
Michael George <michael.g.george@gmail.com>
2022-10-29 19:41:54 +0530

    Test read_file in test_txt_grep, automate

--- automate/tests/test_txt_grep.py
+++ automate/tests/test_txt_grep.py
@@ -1,5 +1,8 @@
 from pathlib import Path
-from scripts.txt_grep import get_filenames
+from scripts.txt_grep import get_filenames, read_file
 
 def test_get_filenames():
     assert get_filenames(Path('./tests')) == [Path('tests/text.txt')]
+
+def test_read_file():
+    assert read_file(Path('./tests/text.txt')) == ['text grep file: some random content\n']
