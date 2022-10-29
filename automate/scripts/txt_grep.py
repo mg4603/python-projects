81fd573229488652ebb39987c969f847b0fc5d54
Michael George <michael.g.george@gmail.com>
2022-10-29 19:41:25 +0530

    Add fn read_file to txt_grep, automate

--- automate/scripts/txt_grep.py
+++ automate/scripts/txt_grep.py
@@ -2,3 +2,8 @@ from pathlib import Path
 
 def get_filenames(path):
     return [filename for filename in path.rglob('*.txt')]
+
+def read_file(path):
+    with path.open('r') as file:
+        lines = file.readlines()
+    return lines
