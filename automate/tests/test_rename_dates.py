4057228e96acdccba00203ae5142fb73c80b6161
Michael George <michael.g.george@gmail.com>
2022-10-30 21:55:23 +0530

    Test contains_american_date in test_rename_dates

---
+++ automate/tests/test_rename_dates.py
@@ -0,0 +1,8 @@
+from scripts.rename_dates import contains_american_date
+
+def test_contain_american_date():
+    assert contains_american_date('12-2-2022asdf.txt')
+    assert contains_american_date('asdf7-12-2022pohasdf.txt')
+    assert not contains_american_date('asdf13-13-2022.txt')
+    assert not contains_american_date('fasd09-33-2022.txt')
+
