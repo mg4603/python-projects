6617a9790ba45ade5e4e2ca26bb59a5c2e33ec98
Michael George <michael.g.george@gmail.com>
2022-10-30 23:36:23 +0530

    Test generate_european_date in test_rename_dates, automate

--- automate/tests/test_rename_dates.py
+++ automate/tests/test_rename_dates.py
@@ -1,5 +1,5 @@
 from pathlib import Path
-from scripts.rename_dates import contains_american_date, get_filenames
+from scripts.rename_dates import contains_american_date, generate_european_date, get_filenames
 
 def test_contain_american_date():
     assert contains_american_date('12-2-2022asdf.txt')
@@ -9,4 +9,9 @@ def test_contain_american_date():
 
 def test_get_filenames():
     assert get_filenames(Path('../scripts/')) == list(Path('../scripts/').rglob('*'))
-    
+
+def test_generate_european_date():
+    assert generate_european_date('asdf12-2-2022asdf.txt') == 'asdf2-12-2022asdf.txt'
+    assert generate_european_date('12-2-2022asdf.txt') == '2-12-2022asdf.txt'
+    assert generate_european_date('asdf2-12-2022asdf.txt') == 'asdf12-2-2022asdf.txt'
+    assert generate_european_date('5-31-2024.txt') == '31-5-2024.txt'
