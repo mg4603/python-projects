2dd700597d4390fe8eed243db19fe8894030db7d
Michael George <michael.g.george@gmail.com>
2022-10-30 22:12:28 +0530

    Test get_filenames to test_rename_dates, automate

--- automate/tests/test_rename_dates.py
+++ automate/tests/test_rename_dates.py
@@ -1,4 +1,5 @@
-from scripts.rename_dates import contains_american_date
+from pathlib import Path
+from scripts.rename_dates import contains_american_date, get_filenames
 
 def test_contain_american_date():
     assert contains_american_date('12-2-2022asdf.txt')
@@ -6,3 +7,6 @@ def test_contain_american_date():
     assert not contains_american_date('asdf13-13-2022.txt')
     assert not contains_american_date('fasd09-33-2022.txt')
 
+def test_get_filenames():
+    assert get_filenames(Path('../scripts/')) == list(Path('../scripts/').rglob('*'))
+    
