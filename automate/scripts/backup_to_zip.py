c3200f1cd9ef51c3d332048ab25f957490c66ca5
Michael George <michael.g.george@gmail.com>
2022-11-01 03:18:37 +0530

    Complete find_latest_backup in backup_to_zip, automate

--- automate/scripts/backup_to_zip.py
+++ automate/scripts/backup_to_zip.py
@@ -1,7 +1,15 @@
 from zipfile import ZipFile
 
 def find_latest_backup(folder_path, backup_path):
-    pass
+    folder_name = folder_path.stem
+    number = 1
+    while True:
+        backup_name = '{}_{}.zip'.format(folder_name, str(number))
+        if not (backup_path / backup_name).exists():
+            break
+        number += 1
+        
+    return backup_name
 
 def create_zip(file_path):
     pass
