9d7a0209b213ec0f079c2a14a68acdbf7c50b2d9
Michael George <michael.g.george@gmail.com>
2022-11-14 13:53:48 +0530

    Add fn to get_toss in debug_coin_toss, automate

--- automate/scripts/debug_coin_toss.py
+++ automate/scripts/debug_coin_toss.py
@@ -1,7 +1,10 @@
-from random import randint
+from random import choice
 
 def get_input(prompt):
     guess = ''
     while guess not in ('heads', 'tails'):
         guess = input(prompt)
+
+def get_toss():
+    return choice(('heads', 'tails'))
 
