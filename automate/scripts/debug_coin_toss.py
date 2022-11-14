14243478f5a7bd48c0321a73bd10b5c8b89bda11
Michael George <michael.g.george@gmail.com>
2022-11-14 14:09:48 +0530

    Add runner code to debug_coin_toss, automate

--- automate/scripts/debug_coin_toss.py
+++ automate/scripts/debug_coin_toss.py
@@ -12,3 +12,20 @@ def get_input(prompt):
 def get_toss():
     return choice(('heads', 'tails'))
 
+toss = get_toss()
+debug('toss: %s' % toss)
+choice = get_input('Guess the coin toss! Enter heads or tails: ')
+debug('choice: %s' % choice)
+
+if toss == choice:
+    print('You got it!')
+else:
+    print('Nope! Guess again!')
+    choice = get_input('Enter heads or tails: ')
+    debug('choice: %s' % choice)
+    if toss == choice:
+        print('You got it!')
+    else:
+        print('Nope. You are really bad at this game.')
+
+
