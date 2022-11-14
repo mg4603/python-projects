7b961064a2bdb489e887fe8f207198c86d6151ab
Michael George <michael.g.george@gmail.com>
2022-11-14 14:05:46 +0530

    Add return to get_input in debug_coin_toss automate

--- automate/scripts/debug_coin_toss.py
+++ automate/scripts/debug_coin_toss.py
@@ -1,9 +1,13 @@
 from random import choice
+from logging import debug, DEBUG, basicConfig, CRITICAL, disable
+basicConfig(level=DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
+# disable(CRITICAL)
 
 def get_input(prompt):
     guess = ''
     while guess not in ('heads', 'tails'):
         guess = input(prompt)
+    return guess
 
 def get_toss():
     return choice(('heads', 'tails'))
