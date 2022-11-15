2821899f4e853e510dc4a367158c99bb2171b135
Michael George <michael.g.george@gmail.com>
2022-11-15 13:19:46 +0530

    Add runner code to maplt in automate

---
+++ automate/scripts/maplt.py
@@ -0,0 +1,15 @@
+'''
+This is what your program does:
+1. Gets a street address from the command line arguments or clipboard
+2. Opens the web browser to the Google Maps page for the address
+This means your code will need to do the following:
+1. Read the command line arguments from sys.argv.
+2. Read the clipboard contents.
+3. Call the webbrowser.open() function to open the web browser.
+'''
+from webbrowser import open
+
+
+if __name__ == '__main__':
+    main()
+    
