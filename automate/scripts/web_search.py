96eefc01b33f3f51089dae868407cc7b208b704a
Michael George <michael.g.george@gmail.com>
2022-11-18 17:51:50 +0530

    Modify main in web_search in automate
    Fixed dict key err in args in main.

--- automate/scripts/web_search.py
+++ automate/scripts/web_search.py
@@ -1,5 +1,10 @@
+#!/usr/bin/env python3
 from webbrowser import open
 from urllib.parse import quote_plus
+from sys import argv, exit
+from pyperclip import paste
+from logging import DEBUG, debug, basicConfig
+basicConfig(level=DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
 
 def parse_args():
     pass
@@ -8,7 +13,7 @@ def main():
     args = parse_args()
     if args['search_terms']:
         url = 'https://www.google.com/search?q=%s'
-        open(url % quote_plus(args['search_term']))
+        open(url % quote_plus(args['search_terms']))
 
 if __name__ == '__main__':
     main()
