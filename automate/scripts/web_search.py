c405a6f6c54215f755fd5454bd4280bd6d614c29
Michael George <michael.g.george@gmail.com>
2022-11-18 17:41:28 +0530

    Complete main fn in web_search in automate

--- automate/scripts/web_search.py
+++ automate/scripts/web_search.py
@@ -1,7 +1,11 @@
 from webbrowser import open
+from urllib.parse import quote_plus
 
 def main():
-    pass
+    args = parse_args()
+    if args['search_terms']:
+        url = 'https://www.google.com/search?q=%s'
+        open(url % quote_plus(args['search_term']))
 
 if __name__ == '__main__':
     main()
