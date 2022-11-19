9dc5a107a47dea23cca3e152f2622da39043d4c3
Michael George <michael.g.george@gmail.com>
2022-11-19 11:43:54 +0530

    Complete main in link_extractor in automate

--- automate/scripts/link_extractor.py
+++ automate/scripts/link_extractor.py
@@ -1,5 +1,15 @@
+from requests import get
+from bs4 import BeautifulSoup
+from webbrowser import open
+
 def main():
-    pass
+    args = parse_args()
+    if args['link']:
+        response = get(args['link'])
+        soup = BeautifulSoup(response.text, 'html.parser')
+        links = [link.get('href') for link in soup.find_all('a')]
+        for link in links:
+            open(link)
 
 if __name__ == '__main__':
     main()
