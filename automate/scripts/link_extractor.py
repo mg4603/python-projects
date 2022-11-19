c92a878bed3d0318dc2ca27c795983fe2b6065dc
Michael George <michael.g.george@gmail.com>
2022-11-19 16:16:53 +0530

    Complete parse_args in link_extractor in automate

--- automate/scripts/link_extractor.py
+++ automate/scripts/link_extractor.py
@@ -1,10 +1,19 @@
 from requests import get
 from bs4 import BeautifulSoup
 from webbrowser import open
+from sys import argv, exit
+from pyperclip import paste
 
 def parse_args():
-    pass
-
+    args = {}
+    if len(argv) > 1:
+        args['link'] = argv[1:]
+    else:
+        args['link'] = paste()
+        if not args['link']:
+            exit('Usage: link_extractor <url>\n\t<url>: url of page from which to extract links\n\tlink_extractor')
+    return args
+    
 def main():
     args = parse_args()
     if args['link']:
