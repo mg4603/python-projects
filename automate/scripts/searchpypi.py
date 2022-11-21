8442c144fd2c637571c41e51fa2afc271741f5fc
Michael George <michael.g.george@gmail.com>
2022-11-21 07:02:34 +0530

    Add runner code to searchpypi in automate

---
+++ automate/scripts/searchpypi.py
@@ -0,0 +1,14 @@
+'''
+This is what your program does:
+1. Gets search keywords from the command line arguments
+2. Retrieves the search results page
+3. Opens a browser tab for each result
+This means your code will need to do the following:
+1. Read the command line arguments from sys.argv.
+2. Fetch the search result page with the requests module.
+3. Find the links to each search result.
+4. Call the webbrowser.open() function to open the web browser.
+'''
+
+if __name__ == '__main__':
+    main()
