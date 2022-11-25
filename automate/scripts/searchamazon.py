1a5f77044bba0c893b034494771f2d4413f10e98
Michael George <michael.g.george@gmail.com>
2022-11-25 09:37:18 +0530

    Add parse_args to searchamazon in automate

--- automate/scripts/searchamazon.py
+++ automate/scripts/searchamazon.py
@@ -2,6 +2,9 @@ from logging import DEBUG, debug, CRITICAL, basicConfig, disable
 basicConfig(level=DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
 # disable(CRITICAL)
 
+def parse_args():
+    pass
+
 def main():
     args = parse_args()
     if args['product']:
