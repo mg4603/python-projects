ed10a0952cb81bcc29e27bfb5050f21a678d4162
Michael George <michael.g.george@gmail.com>
2022-11-25 09:41:39 +0530

    Complete parse_args in searchamazon in automate

--- automate/scripts/searchamazon.py
+++ automate/scripts/searchamazon.py
@@ -1,9 +1,18 @@
 from logging import DEBUG, debug, CRITICAL, basicConfig, disable
+from sys import argv, exit
+from pyperclip import paste
 basicConfig(level=DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
 # disable(CRITICAL)
 
 def parse_args():
-    pass
+    args = {}
+    if len(argv) > 1:
+        args['product'] = ''.join(argv[1:])
+    else:
+        args['product'] = paste()
+        if args['product'] == '':
+            exit('Usage:\nsearchamazon <search_term>\n\t1. <search_term>: term to search for.\n\t2. Term to search for can be on clipboard')
+    return args
 
 def main():
     args = parse_args()
