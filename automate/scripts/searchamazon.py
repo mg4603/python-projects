5e08f32cb151153c9e27071f80677edb6e14a66e
Michael George <michael.g.george@gmail.com>
2022-11-25 09:35:11 +0530

    Complete main in searchamazon in automate

--- automate/scripts/searchamazon.py
+++ automate/scripts/searchamazon.py
@@ -1,5 +1,13 @@
+
+
 def main():
-    pass
+    args = parse_args()
+    if args['product']:
+        response = get_search_response(args['product'])
+        links = get_links(response)
+        debug(links)
+        for link in links:
+            open(f'https://amazon.in{link}')
 
 if __name__ == '__main__':
     main()
