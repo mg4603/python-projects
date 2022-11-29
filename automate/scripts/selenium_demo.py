141fb728ff44f80c5319186e9af459241ce96bd1
Michael George <michael.g.george@gmail.com>
2022-11-29 18:01:12 +0530

    Refactor find_element in selenium_demo in automate

--- automate/scripts/selenium_demo.py
+++ automate/scripts/selenium_demo.py
@@ -4,17 +4,17 @@ from logging import debug, DEBUG, info, INFO, basicConfig, disable
 basicConfig(level=DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
 # disable(DEBUG)
 
-def find_element(browser):
+def find_element_by_class(browser, class_name):
     browser.get('https://inventwithpython.com')
     try:
-        elem = browser.find_element_by_class_name('cover-thumb')
+        elem = browser.find_element_by_class_name(class_name)
         info('Found <%s> element with that class name!' % elem.tag_name)
     except:
         info('Was not able to find an element with that name.')
 
 def main():
     browser = webdriver.Firefox()
-    find_element(browser)
+    find_element_by_class(browser, 'cover-thumb')
 
 if __name__ == '__main__':
     main()
