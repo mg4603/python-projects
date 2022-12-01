944b73b29af86f4c0576d66ab469b61fa84fff83
Michael George <michael.g.george@gmail.com>
2022-12-02 02:45:41 +0530

    Refactor browser to driver in selenium_demo

--- automate/scripts/selenium_demo.py
+++ automate/scripts/selenium_demo.py
@@ -4,42 +4,42 @@ from logging import debug, DEBUG, info, INFO, basicConfig, disable, exception
 basicConfig(level=DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
 # disable(DEBUG)
 
-def find_element_by_class(browser, class_name):
-    browser.get('https://inventwithpython.com')
+def find_element_by_class(driver, class_name):
+    driver.get('https://inventwithpython.com')
     try:
-        elem = browser.find_element_by_class_name(class_name)
+        elem = driver.find_element_by_class_name(class_name)
         info('Found <%s> element with that class name!' % elem.tag_name)
     except:
         info('Was not able to find an element with that name.')
 
-def click_page(browser, text):
-    browser.get('https://inventwithpython.com')
+def click_page(driver, text):
+    driver.get('https://inventwithpython.com')
     try:
-        link_elem = browser.find_element_by_link_text(text)
+        link_elem = driver.find_element_by_link_text(text)
         link_elem.click()
     except Exception as e:
         exception(e)
 
-def form_handler(browser, form_elem_id, form_elem_data, url):
-    browser.get(url)
+def form_handler(driver, form_elem_id, form_elem_data, url):
+    driver.get(url)
     try:
-        field_elem = browser.find_element_by_id(form_elem_id)
+        field_elem = driver.find_element_by_id(form_elem_id)
         field_elem.send_keys(form_elem_data)
     except:
         info('Was not able to find element with that elem_id')
 
-def form_submitter(browser, form_elem_id, url):
-    browser.get(url)
+def form_submitter(driver, form_elem_id, url):
+    driver.get(url)
     try:
-        field_elem = browser.find_element_by_id(form_elem_id)
+        field_elem = driver.find_element_by_id(form_elem_id)
         field_elem.submit()
     except:
         info('Was not able to find element with element id %s' % form_elem_id)
 
 def main():
-    browser = webdriver.Firefox()
-    find_element_by_class(browser, 'cover-thumb')
-    click_page(browser, 'Read Online for Free')
+    driver = webdriver.Firefox()
+    find_element_by_class(driver, 'cover-thumb')
+    click_page(driver, 'Read Online for Free')
 
 if __name__ == '__main__':
     main()
