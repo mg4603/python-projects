0cb2cb549533169f8059e75746dbd90acac682e8
Michael George <michael.g.george@gmail.com>
2022-12-04 00:17:05 +0530

    Complete display_intro in BirthdayParadox in beginner

--- beginner/birthday_paradox.py
+++ beginner/birthday_paradox.py
@@ -12,7 +12,17 @@ class BirthdayParadox:
         pass
 
     def display_intro(self):
-        pass
+        print('''
+############################## Birthday Paradox ##############################
+
+The Birthday Paradox shows us that in a group of N people, the odds that two 
+of them have matching birthdays is surprisingly large. This program does a 
+Monte Carlo simulation (that is, repeated random simulations) to explore this
+concept.
+
+(It's not actually a paradox, it's just a surprising result.)
+'''
+)
 
     def simulate(self):
         pass
