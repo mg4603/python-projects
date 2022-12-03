6d272a7176dd4688b7dcbb931c1e4c4bb2a63ce9
Michael George <michael.g.george@gmail.com>
2022-12-03 16:36:40 +0530

    Complete print_banner in bagels in begnner

--- beginner/bagels.py
+++ beginner/bagels.py
@@ -2,7 +2,20 @@ NUM_DIGITS = 3
 MAX_GUESSES = 10
 
 def print_banner():
-    pass
+    print(f'''
+####################################   Bagels    ####################################
+
+I am thinking of a {NUM_DIGITS}-number with no repeated digits. Try to guess
+what it is. Here are some clues:
+When I say:     That means:
+    Pico        One digit is correct but in the wrong position.
+    Fermi       One digit is correct but in the right position.
+    Bagels      No digit is correct.
+
+For example, if the secret number was 248 and your guess was 843, the clues would be:
+    Fermi Pico.
+''')
+
 
 def main():
     pass
