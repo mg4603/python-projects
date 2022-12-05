efe0625a100d2c9157635174012e8357306bdb9c
Michael George <michael.g.george@gmail.com>
2022-12-06 04:38:32 +0530

    Add DAYS const to CalendarMaker in beginner

--- beginner/calendar_maker.py
+++ beginner/calendar_maker.py
@@ -2,4 +2,7 @@ class CalendarMaker():
     def __init__(self, month, year):
         self.month = month
         self.year = year
-    
+        self.DAYS = (
+            'Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday',
+            'Saturday'
+        )
