8dfcf5895af2a3bfc0c5420e0e0a692748b4b3a1
Michael George <michael.g.george@gmail.com>
2022-12-06 04:19:44 +0530

    Add const LOWER_SYMBOLS to CaesarHacker in beginner

--- beginner/caesarhacker.py
+++ beginner/caesarhacker.py
@@ -1,6 +1,8 @@
 class CaesarHacker:
     def __init__(self):
         self.UPPER_SYMBOLS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+        self.LOWER_SYMBOLS = 'abcdefghijklmnopqrstuvwxyz'
+        
 
 if __name__ == '__main__':
     print('Enter encrypted message to brute force.')
