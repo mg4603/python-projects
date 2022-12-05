696388ec833e1cb8465e9d1d5ef3c19c39d27cb8
Michael George <michael.g.george@gmail.com>
2022-12-05 06:07:22 +0530

    Define BouncingDvd class in beginner

---
+++ beginner/bouncing_dvd.py
@@ -0,0 +1,12 @@
+from sys import exit
+try:
+    from bext import goto
+except:
+    print('This program requires the bext module, which')
+    print('you can install by following the instructions')
+    print('at https://pypi.org/project/bext')
+    exit()
+
+class BouncingDvd:
+    def __inti__(self):
+        pass
