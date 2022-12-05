86f05067fdfc300982822109ad2b9a09bdff015d
Michael George <michael.g.george@gmail.com>
2022-12-06 04:17:47 +0530

    Add runner code to caesarhacker in beginner

--- beginner/caesarhacker.py
+++ beginner/caesarhacker.py
@@ -1,3 +1,9 @@
 class CaesarHacker:
     def __init__(self):
         pass
+
+if __name__ == '__main__':
+    print('Enter encrypted message to brute force.')
+    encrypted_message = input('> ')
+    cracker = CaesarHacker(encrypted_message)
+    cracker.brute_force()
