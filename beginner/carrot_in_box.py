38af1851a671498775a6ac261ea414a17209e43f
Michael George <michael.g.george@gmail.com>
2022-12-07 01:30:49 +0530

    Complete display_intro in CarrotInBox in beginner

--- beginner/carrot_in_box.py
+++ beginner/carrot_in_box.py
@@ -4,4 +4,17 @@ class CarrotInBox:
         self.player2 = ''
     
     def display_intro(self):
-        pass
+        print('-------------------------Carrot In A Box-------------------------')
+        print('\nA silly bluffing game between two human players. Based on the ')
+        print('game from the show: 8 out of 10 cats')
+        print()
+        print('This is a bluffing game for two human players. Each player has a ')
+        print('box. One box has a carrot in it. To win, you must have the box with')
+        print('the carrot in it.')
+        print()
+        print('This is a very simple and silly game.')
+        print()
+        print('The first player looks into their box (the second player must close')
+        print('their eyes during this). The first player then says "There is a ')
+        print('carrot in my box" or "There is not a carrot in my box". The second')
+        print('player then gets to decide if they want to swap boxes or not.')
