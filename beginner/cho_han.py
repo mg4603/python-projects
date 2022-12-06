0076d89c3990e8c512d29fd3d6b4d92c7f816128
Michael George <michael.g.george@gmail.com>
2022-12-07 05:00:13 +0530

    Complete display_intro in ChoHan in beginner

--- beginner/cho_han.py
+++ beginner/cho_han.py
@@ -3,4 +3,10 @@ class ChoHan:
         pass
 
     def display_intro(self):
-        pass
+        print('------------------------------Cho-Han--------------------------')
+        print()
+        print('In this traditional Japanese dice game, two dice are roleld in ')
+        print('a bamboo cup by the dealer sitting on the floor. The player')
+        print('guess if the dice total to an even(cho) or odd(han) number.')
+        print()
+                
