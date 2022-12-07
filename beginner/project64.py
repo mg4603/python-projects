2ccdcb2faed6c74d867a86ce452a085c0be8ab09
Michael George <michael.g.george@gmail.com>
2022-12-08 04:42:27 +0530

    Complete display_intro SevSeg in beginner

--- beginner/project64.py
+++ beginner/project64.py
@@ -3,4 +3,18 @@ class SevSeg:
         pass
 
     def display_intro(self):
-        pass
+        print('--------------------------------------------------------------')
+        print('----------------------------SevSeg----------------------------')
+        print('A seven-segment number display module, used by the Countdown')
+        print('and Digital Clock programs.')
+        print()
+        print(
+'''A labeled seven-segment display, with each segment labeled A-G:
+ _A_
+|   |      Each digit is a seven-segment display:
+F   B      _        _   _        _   _   _   _   _
+|_G_|     | |   |   _|  _|  |_| |_  |_    | |_| |_|         
+|   |     |_|   |  |_   _|    |  _| |_|   | |_|  _|
+E   C
+|_D_|''')
+
