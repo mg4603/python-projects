92153537b4d3d470821cf04c57aa402877a66be5
Michael George <michael.g.george@gmail.com>
2022-12-07 19:22:23 +0530

    Complete main in ClickBait in beginner

--- beginner/click_bait.py
+++ beginner/click_bait.py
@@ -1,3 +1,5 @@
+from random import randint, choice
+
 class ClickBait:
     def __init__(self):
         self.OBJECT_PRONOUNS = ['Her', 'Him', 'Them']
@@ -27,5 +29,40 @@ class ClickBait:
         print()
         print('Our website needs to trick people into looking at ads!')
 
-    def main():
-        pass
+    def main(self):
+        number_of_headlines = self.get_num_of_headlines()
+
+        for i in range(number_of_headlines):
+            clickbait_type = randint(1, 8)
+
+            if clickbait_type == 1:
+                headline = self.generate_are_millennials_killing_headline()
+            elif clickbait_type == 2:
+                headline = self.generate_what_you_dont_know_headline()
+            elif clickbait_type == 3:
+                headline = self.generate_big_companies_hate_headline()
+            elif clickbait_type == 4:
+                headline = self.generate_you_wont_believe_headline()
+            elif clickbait_type == 5:
+                headline = self.generate_you_dont_want_to_know_headline()
+            elif clickbait_type == 6:
+                headline = self.generate_gift_idea_headline()
+            elif clickbait_type == 7:
+                headline = self.generate_reasons_why_headline()
+            elif clickbait_type == 8:
+                headline = self.generate_job_automated_headline()
+            
+            print(headline)
+        print()
+
+        website = choice(
+            [
+                'wobsite', 'blag', 'Facebuuk', 'Googles', 'Fasebook', 
+                'teewide', 'Pastagram'
+            ]
+        )
+
+        when = choice(self.WHEN).lower()
+        print(
+            'Post these to our {} {} or you\'re fired!'.format(website, when)
+            )
