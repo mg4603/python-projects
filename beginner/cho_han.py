6665944aa8c17aa52465bb982a7acbce5e310da6
Michael George <michael.g.george@gmail.com>
2022-12-07 07:04:15 +0530

    Complete constructor in ChoHan in beginner

--- beginner/cho_han.py
+++ beginner/cho_han.py
@@ -1,11 +1,12 @@
 class ChoHan:
     def __init__(self):
-        pass
+        self.money = 5000
+        
 
     def display_intro(self):
         print('------------------------------Cho-Han--------------------------')
         print()
-        print('In this traditional Japanese dice game, two dice are roleld in ')
+        print('In this traditional Japanese dice game, two dice are rolled in ')
         print('a bamboo cup by the dealer sitting on the floor. The player')
         print('guess if the dice total to an even(cho) or odd(han) number.')
         print()
