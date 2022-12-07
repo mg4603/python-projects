5cc5274aa3224e4c552f086dbc20aaf9d2a1c949
Michael George <michael.g.george@gmail.com>
2022-12-08 05:06:18 +0530

    Complete get_sev_seg in SevSeg in beginner

--- beginner/sevseg.py
+++ beginner/sevseg.py
@@ -19,4 +19,67 @@ E     C
 |__D__|''')
 
     def get_sev_seg(self, number, min_width=0):
-        pass
+        number = str(number).zfill(min_width)
+
+        rows = ['', '', '']
+        for i, numeral in enumerate(number):
+            if numeral == '.':
+                rows[0] += ' '
+                rows[1] += ' '
+                rows[2] += '.'
+                continue        #skip the space in between digits.
+            elif numeral == '-':
+                rows[0] += '    '
+                rows[1] += ' __ '
+                rows[2] += '    '
+            elif numeral == '0':
+                rows[0] += ' __ '
+                rows[1] += '|  |'
+                rows[2] += '|__|'
+            elif numeral == '1':
+                rows[0] += '    '
+                rows[1] += '   |'
+                rows[2] += '   |'
+            elif numeral == '2':
+                rows[0] += ' __ '
+                rows[1] += ' __|'
+                rows[2] += '|__'
+            elif numeral == '3':
+                rows[0] += ' __ '
+                rows[1] += ' __|'
+                rows[2] += ' __|'
+            elif numeral == 4:
+                rows[0] += '    '
+                rows[1] += '|__|'
+                rows[2] += '   |'
+            elif numeral == 5:
+                rows[0] += ' __ '
+                rows[1] += '|__ '
+                rows[2] += ' __|'
+            elif numeral == 6:
+                rows[0] += ' __ '
+                rows[1] += '|__ '
+                rows[2] += '|__|'
+            elif numeral == 7:
+                rows[0] += ' __ '
+                rows[1] += '   |'
+                rows[2] += '   |'
+            elif numeral == 8:
+                rows[0] += ' __ '
+                rows[1] += '|__|'
+                rows[2] += '|__|'
+            elif numeral == 9:
+                rows[0] += ' __ '
+                rows[1] += '|__|'
+                rows[2] += ' __|'
+            
+            # add a space(in between numerals) if this isn't the last number
+            if i != len(number) - 1:
+                rows[0] += ' '
+                rows[1] += ' '
+                rows[2] += ' '
+        
+        return '\n'.join(rows)
+
+
+
