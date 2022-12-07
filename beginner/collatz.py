39ec941011b5a7416f592ef35fcb5cec61f38e1d
Michael George <michael.g.george@gmail.com>
2022-12-08 00:43:53 +0530

    Complete display_intro in Collatz in beginner

--- beginner/collatz.py
+++ beginner/collatz.py
@@ -3,4 +3,17 @@ class Collatz:
         self.number = number
 
     def display_intro(self):
-        pass
+        print('--------------------------------------------------------------')
+        print('-----------Collatz Sequence, or, the 3n + 1 Problem-----------')
+        print('--------------------------------------------------------------')
+        print('The collatz sequence is a sequence of numbers produced from a ')
+        print('starting number n, following three rules:')
+        print('\t1) If n is even, the next number n is n/2')
+        print('\t2) If n is odd, the next number n is 3 * n + 1')
+        print('\t3) If n is 1, stop. Otherwise, repeat')
+        print()
+        print("It is generally thought, but so far not mathematically proven,")
+        print('that every starting number eventually terminates at 1.')
+        print()
+    
+    
