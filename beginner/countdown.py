466a00f9eac5188fdadc22364d58b5a5b0ebe209
Michael George <michael.g.george@gmail.com>
2022-12-08 05:15:30 +0530

    Complete constructor in Countdown in beginner

--- beginner/countdown.py
+++ beginner/countdown.py
@@ -1,5 +1,6 @@
 from sevseg import SevSeg
 
 class Countdown:
-    def __init__(self, time, message):
-        pass
+    def __init__(self, hours, mins, secs, message):
+        self.hours, self.mins, self.secs = hours, mins, secs
+        self.message = message
