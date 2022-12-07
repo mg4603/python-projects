ccf0cb1cfc5541951edc03e2b78dbeac2ad225cb
Michael George <michael.g.george@gmail.com>
2022-12-08 05:15:58 +0530

    Add display_intro to Countdown in beginner

--- beginner/countdown.py
+++ beginner/countdown.py
@@ -4,3 +4,6 @@ class Countdown:
     def __init__(self, hours, mins, secs, message):
         self.hours, self.mins, self.secs = hours, mins, secs
         self.message = message
+    
+    def display_intro(self):
+        pass
