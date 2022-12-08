dff4d1a0015c5767af6cdb2685eaf5270f75b55c
Michael George <michael.g.george@gmail.com>
2022-12-08 23:40:08 +0530

    Complete display_intro in DiceMath in beginner

--- beginner/dicemath.py
+++ beginner/dicemath.py
@@ -3,7 +3,14 @@ class DiceMath:
         pass
 
     def display_intro(self):
-        pass
+        print('---------------------------------------------------------------')
+        print('---------------------------Dice Math---------------------------')
+        print('---------------------------------------------------------------')
+        print()
+        print('Add up the sides of all  the dice displayed on the screen. You ')
+        print('30 seconds to answer as many as possible. You get 4 points for ')
+        print('each correct answer and lose 1 point for each incorrect answer.')
+        print()
 
     def main(self):
         pass
