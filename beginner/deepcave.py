e5dd4bada1203779ad31e5807f047215b69adc2f
Michael George <michael.g.george@gmail.com>
2022-12-08 18:00:19 +0530

    Complete animate in DeepCave in beginner

--- beginner/deepcave.py
+++ beginner/deepcave.py
@@ -1,4 +1,6 @@
 from time import sleep
+from sys import exit
+from random import randint
 
 class DeepCave:
     def __init__(self):
@@ -19,4 +21,34 @@ class DeepCave:
         pass
 
     def animate(self):
-        pass
+        l_width = self.get_l_width()
+        g_width = self.get_g_width()  
+
+        while True:
+            try:
+                r_width = self.WIDTH - g_width - l_width
+                print(
+                    '{}{}{}'.format(
+                        ('#' * l_width), (' ' * g_width), ('#' * r_width)
+                    )
+                )
+
+                dice_roll = randint(1, 6)
+                if dice_roll == 1 and l_width > 1:
+                    l_width -= 1
+                elif dice_roll == 2 and l_width + g_width < self.WIDTH - 1:
+                    l_width += 1
+                else:
+                    pass
+
+                dice_roll = randint(1, 6)
+                if dice_roll == 1 and g_width > 1:
+                    g_width -= 1
+                elif dice_roll == 2 and l_width + g_width < self.WIDTH - 1:
+                    g_width += 1
+                else:
+                    pass
+                sleep(self.PAUSE_AMOUNT)
+            except KeyboardInterrupt:
+                exit()
+
