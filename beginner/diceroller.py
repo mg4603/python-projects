c2cee7d15413ae4574a5f46de577194f6a114eaa
Michael George <michael.g.george@gmail.com>
2022-12-09 04:25:19 +0530

    Complete input_handler in DiceRoller in beginner

--- beginner/diceroller.py
+++ beginner/diceroller.py
@@ -1,3 +1,5 @@
+from sys import exit
+
 class DiceRoller:
     def __init__(self):
         pass
@@ -19,7 +21,46 @@ class DiceRoller:
         print('     QUIT quits the program')
 
     def input_handler(self):
-        pass
-    
+        dice_str = input('> ')
+        if dice_str.upper() == 'QUIT':
+            exit()
+
+        # clean up dice_str
+        dice_str = dice_str.lower().replace(' ', '')
+        
+        d_index = dice_str.find('d')
+        if d_index == -1:
+            raise Exception('Missing the "d" character.')
+        
+        # get number of dice
+        number_of_dice = dice_str[:d_index]
+        if not number_of_dice.isdecimal():
+            raise Exception('Missing the number of dice.')
+        self.number_of_dice = int(number_of_dice)
+
+        mod_index = dice_str.find('+')
+        if mod_index == -1:
+            mod_index = dice_str.find('-')
+        
+        if mod_index == -1:
+            number_of_sides = dice_str[d_index + 1:]
+        else:
+            number_of_sides = dice_str[d_index + 1: mod_index]
+        
+        if not number_of_sides.isdecimal():
+            raise Exception('Missing number of sides.')
+        self.number_of_sides = int(number_of_sides)
+
+        # modifier amt
+        if mod_index == -1:
+            mod_amount = 0
+        else:
+            mod_amount = dice_str[mod_index + 1:]
+            if not mod_amount.isdecimal():
+                raise Exception('Missing modification amount')
+            self.mod_amount = int(mod_amount)
+            if dice_str[mod_index] == '-':
+                self.mod_amount = -self.mod_amount
+        
     def main():
         pass
