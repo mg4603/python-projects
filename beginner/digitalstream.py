57fff173a639ec9fa0dd0c8e37e8addddc59da99
Michael George <michael.g.george@gmail.com>
2022-12-09 09:30:56 +0530

    Complete animation in DigitalStream in beginner

--- beginner/digitalstream.py
+++ beginner/digitalstream.py
@@ -1,5 +1,8 @@
 from shutil import get_terminal_size
 from time import sleep
+from sys import exit, stdout
+from random import choice, random, randint
+
 class DigitalStream:
     def __init__(self):
         self.MIN_STREAM_LENGTH = 6
@@ -20,7 +23,26 @@ class DigitalStream:
         sleep(2)
 
     def animation(self):
-        pass
+        try:
+            columns = [0] * self.WIDTH
+            while True:
+                for i in range(self.WIDTH):
+                    if columns[i] == 0:
+                        if random() <= self.DENSITY:
+                            columns[i] = randint(
+                                self.MIN_STREAM_LENGTH, self.MAX_STREAM_LENGTH
+                            )
+                        
+                    if columns[i] > 0:
+                        print(choice(self.STREAM_CHARS), end='')
+                        columns[i] -= 1
+                    else:
+                        print(' ', end='')
+                print()
+                stdout.flush()
+                sleep(self.PAUSE)
+        except KeyboardInterrupt:
+            exit()
 
 if __name__ == '__main__':
     digital_stream = DigitalStream()
