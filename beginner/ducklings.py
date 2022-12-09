5cfa93eb90aad3954b5b52f0a13852ab802be3e9
Michael George <michael.g.george@gmail.com>
2022-12-09 21:39:01 +0530

    Complete animate in DucklingsAnimation in beginner

--- beginner/ducklings.py
+++ beginner/ducklings.py
@@ -1,5 +1,7 @@
 from time import sleep
 from shutil import get_terminal_size
+from random import random, choice
+from sys import stdout, exit
 
 class Duckling:
     LEFT = 'left'
@@ -40,7 +42,27 @@ class DucklingsAnimation:
         sleep(2)
 
     def animate(self):
-        pass
+        duckling_lanes = [None] * \
+            (self.TERMINAL_WIDTH // self.DUCKLING_WIDTH)
+
+        try:
+            while True:
+                for lane_num, duckling_obj in enumerate(duckling_lanes):
+                    if (duckling_obj == None and random() <= self.DENSITY):
+                        duckling_obj = Duckling()
+                        duckling_lanes[lane_num] = duckling_obj
+                    
+                    if duckling_obj != None:
+                        print(duckling_obj.get_next_body_part(), end='')
+                        if duckling_obj.part_to_display_next == None:
+                            duckling_lanes[lane_num] = None
+                    else:
+                        print(' ' * self.DUCKLING_WIDTH, end='')                    
+                print()
+                stdout.flush()
+                sleep(self.PAUSE)
+        except KeyboardInterrupt:
+            exit()
 
 if __name__ == '__main__':
     animator = DucklingsAnimation()
