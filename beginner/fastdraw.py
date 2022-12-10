5b64137d340e34d12c62d53f0b4a1ed8289ca4d9
Michael George <michael.g.george@gmail.com>
2022-12-10 21:55:36 +0530

    Complete display_intro in FastDraw in beginner

--- beginner/fastdraw.py
+++ beginner/fastdraw.py
@@ -3,7 +3,14 @@ class FastDraw:
         pass
 
     def display_intro(self):
-        pass
+        print('-------------------------------------------------------------')
+        print('--------------------------Fast Draw--------------------------')
+        print('-------------------------------------------------------------')
+        print('Time to test your reflexes and see if you are the fastest ')
+        print('draw in the west!')
+        print('When you see "DRAW" you have 0.3 seconds to press Enter.')
+        print('But you lose if you press Enter before "DRAW" appears.')
+        print()
 
     def main(self):
         pass
