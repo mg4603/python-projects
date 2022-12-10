a7f57f8d93de1bbd2dd6eeff5e2323da9f0bd775
Michael George <michael.g.george@gmail.com>
2022-12-10 22:22:52 +0530

    Add display_intro to Fibonacci in beginner

--- beginner/fibonacci.py
+++ beginner/fibonacci.py
@@ -1,3 +1,16 @@
 class Fibonacci:
     def __init__(self):
         pass
+    
+    def display_intro(self):
+        print('--------------------------------------------------------------')
+        print('----------------------Fibonacci Sequence----------------------')
+        print('--------------------------------------------------------------')
+        print()
+        print('The Fibonacci sequence begins with 0 and 1, and the next')
+        print('number is the sum of the previous two numbers. The sequence')
+        print('continues forever: ')
+        print()
+        print('{}\n{}\n\n'.format(
+                '0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377,',
+                '610, 987, 1597...'))
