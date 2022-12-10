8f9a06950d54fa6b48e7d9a6d6560bc3b14e09c7
Michael George <michael.g.george@gmail.com>
2022-12-10 19:04:00 +0530

    Complete constructor in FactorFinder in beginner

--- beginner/factorfinder.py
+++ beginner/factorfinder.py
@@ -1,6 +1,10 @@
 class FactorFinder:
-    def __init__(self):
-        pass
+    def __init__(self, number):
+        if is_int(number):
+            self.number = number
+        else:
+            raise Exception('Number to factorize needs to be an integer.')
+
     
     def display_intro(self):
         print('-------------------------------------------------------------')
