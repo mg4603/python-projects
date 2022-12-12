dac2e18ce41be3b0223b1420a5f42cba72aed401
Michael George <michael.g.george@gmail.com>
2022-12-13 01:24:52 +0530

    Add display_intro to GuessTheNumber in guess, beginner

--- beginner/guess.py
+++ beginner/guess.py
@@ -1,3 +1,11 @@
 class GuessTheNumber:
     def __init__(self):
         pass
+    
+    def display_intro(self):
+        print('--------------------------------------------------------------')
+        print('---------------------- Guess the Number ----------------------')
+        print('--------------------------------------------------------------')
+        print()
+        print('I am thinking of a number between 1 and 100.')
+        print()
