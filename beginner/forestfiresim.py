b822ba9d7df9f788739fb2933f9b161c91aca503
Michael George <michael.g.george@gmail.com>
2022-12-12 06:52:27 +0530

    Define ForestFireSim class in beginner

---
+++ beginner/forestfiresim.py
@@ -0,0 +1,13 @@
+from sys import exit
+from random import choice
+try:
+    from bext import clear, fg, bg
+except ImportError:
+    print('This program requires the bext module to run.')
+    print('Installation instructions for the bext module')
+    print('can be found at https://pypi.org/project/Bext')
+    exit()
+
+class ForestFireSim:
+    def __init__(self):
+        pass
