e07f9dc554bee7488ff5a24e6d2d9663b26a59a5
Michael George <michael.g.george@gmail.com>
2022-12-12 22:47:50 +0530

    Add display_intro to fourinrow in beginner

--- beginner/fourinrow.py
+++ beginner/fourinrow.py
@@ -1,3 +1,12 @@
 class FourInRow:
     def __init__(self):
         pass
+
+    def display_intro(self):
+        print('-------------------------------------------------------------')
+        print('----------------------- Four In A Row -----------------------')
+        print()
+        print('Two players take turns dropping tiles into one of the seven')
+        print('columns, trying to make four in a row horizontally, vertically')
+        print('or diagonally.')
+        
