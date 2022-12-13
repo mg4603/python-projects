955ff56919e1f3b8d1ba2777df05166e1d43446c
Michael George <michael.g.george@gmail.com>
2022-12-13 06:04:55 +0530

    Add display_intro to HackingGame in beginner

--- beginner/hacking.py
+++ beginner/hacking.py
@@ -12,3 +12,17 @@ class HackingGame:
             print('File {} not found in current working dir'.format(
                 file_name
             ))
+    
+    def display_intro(self):
+        print('--------------------------------------------------------')
+        print('------------------- Hacking Minigame -------------------')
+        print('--------------------------------------------------------')
+        print()
+        print('Find the password in the computer\'s memory. You are ')
+        print('given clues after each guess. For example if the ')
+        print('secret password is MONITOR but the player guessed')
+        print('CONTAIN, they are given the hint that 2 out of 7')
+        print('letters were correct, because both MONITOR and')
+        print('CONTAIN have the letters O and N in their 2nd and 3rd')
+        print('letter. You get four guesses.\n')
+        
