bcb3d7df48437d693f489368cd629ae8fc13b308
Michael George <michael.g.george@gmail.com>
2022-12-13 06:21:33 +0530

    Add main to HackingGame in beginner

--- beginner/hacking.py
+++ beginner/hacking.py
@@ -1,4 +1,6 @@
 from pathlib import Path
+from random import choice
+from sys import exit
 
 class HackingGame:
     GARBAGE_CHARS = '~!@#$%^&*()_+-={}[]|;:,.<>?/'
@@ -12,6 +14,12 @@ class HackingGame:
             print('File {} not found in current working dir'.format(
                 file_name
             ))
+        
+        self.game_words = []
+        self.computer_memory = ''
+        self.secret_password = ''
+        self.player_guess = ''
+        self.num_matches = 0
     
     def display_intro(self):
         print('--------------------------------------------------------')
@@ -25,4 +33,27 @@ class HackingGame:
         print('letters were correct, because both MONITOR and')
         print('CONTAIN have the letters O and N in their 2nd and 3rd')
         print('letter. You get four guesses.\n')
-        
+    
+    def main(self):
+        input('Press Enter to begin...')
+        self.get_game_words()
+        self.get_computer_memory_string()
+        self.secret_password = choice(self.game_words)
+
+        print(self.computer_memory)
+        for tries_remaining in range(4, 0, -1):
+            print('Enter password: ({} tries remaining)'.format(
+                tries_remaining
+            ))
+            self.get_player_guess()
+            if self.player_guess == self.secret_password:
+                print('A C C E S S  G R A N T E D')
+                return
+            else:
+                self.get_num_matching_letters()
+                print('Access Denied ({}/7 correct)'.format(
+                    self.num_matches
+                ))
+        print('Out of tries. Secret password was {}.'.format(
+            self.secret_password
+        ))
