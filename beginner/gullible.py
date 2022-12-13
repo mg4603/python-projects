f21f123dbb43bc54054f5edcab0e8e1a8375bb21
Michael George <michael.g.george@gmail.com>
2022-12-13 05:36:20 +0530

    Add get_response to Gullible in gullible in beginner

--- beginner/gullible.py
+++ beginner/gullible.py
@@ -8,5 +8,12 @@ class Gullible:
         print('------------------------------------------------------------')
         print()
     
+    def get_response(self):
+        print('{}\n{}'.format(
+            'Do you want to know how to keep a gullible', 
+            'person busy for hours? (Y/N)'
+        ))
+        self.response = input('> ').strip().upper()
+    
     def main(self):
         pass
