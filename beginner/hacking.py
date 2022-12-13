dfea3a035efbec76ba4355c41b8771b344f055c1
Michael George <michael.g.george@gmail.com>
2022-12-13 05:53:38 +0530

    Add constants to HackingGame in beginner

--- beginner/hacking.py
+++ beginner/hacking.py
@@ -1,4 +1,14 @@
+from pathlib import Path
 
 class HackingGame:
-    def __init__(self):
-        pass
+    GARBAGE_CHARS = '~!@#$%^&*()_+-={}[]|;:,.<>?/'
+
+    def __init__(self, file_name):
+        try:
+            with Path(file_name).open('w') as file:
+                self.WORDS = file.readlines()
+            self.WORDS = [word.strip().upper() for word in self.WORDS]
+        except FileNotFoundError:
+            print('File {} not found in current working dir'.format(
+                file_name
+            ))
