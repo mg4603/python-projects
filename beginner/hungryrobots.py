71af47e1acdcd73e2f29703a6cd1e147284c5feb
Michael George <michael.g.george@gmail.com>
2022-12-16 21:44:02 +0530

    Add display_board to HungryRobots in beginner

--- beginner/hungryrobots.py
+++ beginner/hungryrobots.py
@@ -38,7 +38,24 @@ class HungryRobots:
     NUM_TELEPORTS = 2
     NUM_DEAD_ROBOTS = 10
     NUM_ROBOTS = 10
+
     def __init__(self):
         self.board = {}
         self.robots = []
         self.player_position = None
+    
+    def display_board(self):
+        for y in range(self.HEIGHT):
+            for x in range(self.WIDTH):
+                if self.board[(x, y)] == self.WALL:
+                    print(self.WALL, end='')
+                elif self.board[(x, y)] == self.DEAD_ROBOT:
+                    print(self.DEAD_ROBOT, end='')
+                elif (x, y) == self.player_position:
+                    print(self.PLAYER, end='')
+                elif (x, y) in self.robots:
+                    print(self.ROBOT, end='')
+                else:
+                    print(self.EMPTY_SPACE, end='')
+
+            print()
