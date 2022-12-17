d825ad27a92c659240233f1b5b9dd9b1249a7ba1
Michael George <michael.g.george@gmail.com>
2022-12-18 02:57:30 +0530

    Add const SUSPECTS to Jaccuse in beginner

--- beginner/jaccuse.py
+++ beginner/jaccuse.py
@@ -1,3 +1,12 @@
+from random import randint, choice, shuffle
+from sys import exit
+from time import time
+
 class Jaccuse:
+    SUSPECTS = [
+        'DUKE HAUTDOG', 'MAXIMUM POWERS', 'BILL MONOPOLIS', 'SENATOR SCHMEAR',
+        'MRS. FEATHERTOSS', 'DR. JEAN SPLICER', 'RAFFLES THE CLOWN', 
+        'ESPRESSA TOFFEEPOT', 'CECIL EDGAR VANDERTON'
+    ]
     def __init__(self):
         pass
