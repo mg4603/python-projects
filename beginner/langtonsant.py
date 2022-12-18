b927245846131aee04efd9ed48d5bbc9f67ea979
Michael George <michael.g.george@gmail.com>
2022-12-19 00:01:41 +0530

    Define LangtonsAnt class in langtonsant in beginner

---
+++ beginner/langtonsant.py
@@ -0,0 +1,48 @@
+'''
+LangtonsAnt
+const:
+    WIDTH:              terminal width
+    HEIGHT:             terminal height
+
+    ANT_UP
+    ANT_DOWN
+    ANT_LEFT
+    ANT_RIGHT
+
+    ANT_COLOR
+    BLACK_TILE
+    WHITE_TILE
+
+    PAUSE_AMOUNT:       0.1
+    NUMBER_OF_ANTS:     10
+
+    NORTH:              North
+    SOUTH:              south
+    EAST:               east
+    WEST:               west
+
+attributes:
+    board
+    changed_tiles
+    ants
+
+methods:
+    main
+    display_intro
+    display_board
+'''
+
+try:
+    from bext import fg, bg, clear, goto
+except ImportError:
+    print('This program requires the bext module to run.')
+    print('Installation instructions can be found at')
+    print('https://pypi.org/project/Bext')
+
+from sys import exit
+from random import choice, randint
+from time import sleep
+
+class LangtonsAnt:
+    def __init__(self):
+        pass
