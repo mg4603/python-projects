9c8fdc08dbe60ca186f8d25a1d8d6d7afc44bd48
Michael George <michael.g.george@gmail.com>
2022-12-18 05:30:08 +0530

    Complete constructor in Jaccuse in beginner

--- beginner/jaccuse.py
+++ beginner/jaccuse.py
@@ -15,8 +15,7 @@ attributes:
     current_location
     clues
     zophie_clues
-    known_suspects
-    known_items
+    known_suspects_and_items
 methods:
     display_intro
     get_clues
@@ -25,7 +24,7 @@ methods:
     main
 '''
 
-from random import randint, choice, shuffle
+from random import randint, choice, shuffle, sample
 from sys import exit
 from time import time
 
@@ -49,4 +48,28 @@ class Jaccuse:
     PLACE_FIRST_LETTERS = {}
     LONGEST_PLACE_NAME_LENGTH = 0
     def __init__(self):
-        pass
+        for place in self.PLACES:
+            self.PLACE_FIRST_LETTERS[place[0]] = place
+            if len(place) > self.LONGEST_PLACE_NAME_LENGTH:
+                self.LONGEST_PLACE_NAME_LENGTH = len(place)
+        
+        assert len(self.SUSPECTS) == 9
+        assert len(self.PLACES) == 9
+        assert len(self.SUSPECTS) == 9
+        assert len(self.LONGEST_PLACE_NAME_LENGTH.keys()) == len(self.PLACES)
+        self.known_suspects_and_items = []
+        self.clues = {}
+        self.zophie_clues = {}
+        self.accusations_left = 3
+        self.visited_places = {}
+        self.accused_suspects = []
+        
+        shuffle(self.SUSPECTS)
+        shuffle(self.PLACES)
+        shuffle(self.ITEMS)
+
+        self.liars = sample(self.SUSPECTS, randint(3, 4))
+        self.current_location = 'TAXI'
+        self.culprit = choice(self.SUSPECTS)
+
+    
