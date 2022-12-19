f6927530bfbacd2a5260d6c0be37ca78a2e49248
Michael George <michael.g.george@gmail.com>
2022-12-19 21:57:05 +0530

    Complete leetspeak in beginner

--- beginner/leetspeak.py
+++ beginner/leetspeak.py
@@ -16,7 +16,7 @@ mapping
 '''
 from random import random, choice
 try:
-    from pyperclip import copy
+    from pyperclip import copy, paste
 except ImportError:
     pass
 
@@ -37,17 +37,24 @@ class LeetSpeak:
         'v': ['\\/']   
     }
     LEET_MAP = {
-         '4@/-\\': 'a',
+         '4': 'a',
+         '@': 'a',
+         '/-\\': 'a',
          '(': 'c',
          '|)': 'd',
          '3': 'e',
          'ph': 'f',
-         ']-[|-|': 'h',
-         '1!|': 'i',
+         ']-[': 'h',
+         '|-|': 'h',
+         '1': 'i',
+         '!': 'i',
+         '|': 'i',
          ']<': 'k',
          '0': 'o',
-         '$5': 's',
-         '7+': 't',
+         '$': 's',
+         '5': 's',
+         '7': 't',
+         '+': 't',
          '|_|': 'u',
          '\\/': 'v' 
     }
@@ -72,20 +79,29 @@ class LeetSpeak:
         try:
             copy(leetspeak)
             print('(Copied leetspeak to clipboard).')
-        except:
+        except NameError:
             pass
     
     def leetspeak_to_english(self, leet_msg):
         english_msg = ''
-        for char in leet_msg:
-            is_leet_char = False
-            for key in self.LEET_MAP.keys():
-                if char in key:
-                    english_msg += self.LEET_MAP[key]
-            
-            if not is_leet_char:
-                english_msg += char
-        return english_msg
+        for char in self.LEET_MAP:
+            while char in leet_msg:
+                width = len(char)
+                start = leet_msg.find(char)
+                prev_msg = leet_msg
+                leet_msg = '{}{}{}'.format(
+                    prev_msg[: start],
+                    self.LEET_MAP[char],
+                    prev_msg[start + width:]
+                )
+
+        english_msg = leet_msg
+        print(english_msg)
+        try:
+            copy(english_msg)
+            print('(Copied english message to clipboard).')
+        except NameError:
+            pass
 
 if __name__ == '__main__':
     print('Enter your leet message:')
@@ -93,3 +109,4 @@ if __name__ == '__main__':
     print()
     leetEngine = LeetSpeak()
     leetEngine.english_to_leetspeak(message)
+    leetEngine.leetspeak_to_english(paste())
