cb6cb1edd125520f170fb2c32c67bfa501e7f6de
Michael George <michael.g.george@gmail.com>
2022-12-20 14:37:18 +0530

    Define LuckyStars class in luckystars in beginner

---
+++ beginner/luckystars.py
@@ -0,0 +1,34 @@
+'''
+class LuckyStars
+    const:
+        STAR_FACE
+        SKULL_FACE
+        QUESTION_FACE
+    
+        GOLD
+        SILVER
+        BRONZE
+    
+        FACE_WIDTH
+        FACE_HEIGHT
+    
+    attributes:
+        player_names
+        player_scores
+        winners
+    
+    methods:
+        display_intro
+        main
+        setup_round
+        get_players
+        display_scores
+        get_winner
+        display_winner
+        ask_continue_turn
+
+'''
+
+class LuckyStars:
+    def __init__(self):
+        pass
