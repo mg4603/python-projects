94b42bf66b31e0ce35209d0d9595ece725bc9703
Michael George <michael.g.george@gmail.com>
2022-12-21 10:27:04 +0530

    Define MagicFortuneBall class in beginner

---
+++ beginner/magic_fortune_ball.py
@@ -0,0 +1,34 @@
+'''
+const:
+    REPLIES = [
+        'let me think on this...',
+        'an interesting question...',
+        'hmm...are you sure you want to know..?',
+        'do you think some things are best left unknown..?',
+        'i might tell you, but you might not like the answer...',
+        'yes... no... maybe... i will think on it...',
+        'and what will you do when you know the answer? we shall see...',
+        'i shall consult my vision...',
+        'you may want to sit down for this...'
+    ]
+    ANSWERS = [
+        'yes, for sure',
+        'my answer is no',
+        'ask me later',
+        'i am programmed to say yes',
+        'the stars say yes, but i say no',
+        'i dunno maybe',
+        'focus and ask once more',
+        'doubtful, very doubtful',
+        'affirmative',
+        'yes, thought you may not like it',
+        'no, but you may wish it was so'
+    ]
+methods:
+    main
+    slow_space_print
+'''
+
+class MagicFortuneBall:
+    def __init__(self):
+        pass
