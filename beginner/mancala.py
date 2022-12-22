c289b5842332bb24685917c1d2fc601a4d2824ca
Michael George <michael.g.george@gmail.com>
2022-12-22 07:35:03 +0530

    Add const OPPOSITE_PIT to Mancala in beginner

--- beginner/mancala.py
+++ beginner/mancala.py
@@ -21,6 +21,10 @@ methods:
 class Mancala:
     PLAYER_1_PITS = ('A', 'B', 'C', 'D', 'E', 'F')
     PLAYER_2_PITS = ('G', 'H', 'I', 'J', 'K', 'L')
+    OPPOSITE_PIT = {
+        'A': 'G', 'B': 'H', 'C': 'I', 'D': 'J', 'E': 'K', 'F': 'L',
+        'G': 'A', 'H': 'B', 'I': 'C', 'J': 'D', 'K': 'E', 'L': 'F'
+    }
     def __init__(self):
         self.board = {}
         self.player_turn = None
