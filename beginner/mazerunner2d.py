2201e178238f9d5b87faa803a0fe525f3ac6a905
Michael George <michael.g.george@gmail.com>
2022-12-23 03:24:13 +0530

    Add const WALL to MazeRunner2D in beginner

---
+++ beginner/mazerunner2d.py
@@ -0,0 +1,27 @@
+'''
+const:
+    WALL:               representation of wall in maze file
+    EMPTY:              representation of empty in maze file
+    START:              representation of start in maze file
+    EXIT:               representation of exit in maze file
+    PLAYER:             display symbol of player
+    BLOCK:              display symbol of wall
+attributes:
+    height
+    width
+    maze
+    player_x
+    player_y
+    exit_x
+    exit_y
+methods:
+    display_maze
+    get_player_move
+    move_player
+    check_winner
+    main
+'''
+
+class MazeRunner2D:
+    WALL = '#'
+    
