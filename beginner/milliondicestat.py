54d51339965ba869f052af268219e5dc2d65a99a
Michael George <michael.g.george@gmail.com>
2022-12-26 13:52:29 +0530

    Add NUMBER_OF_SIMULATIONS to MillionDiceSim in beginner

--- beginner/milliondicestat.py
+++ beginner/milliondicestat.py
@@ -2,6 +2,7 @@ from random import randint
 from time import time
 
 class MillionDiceSim:
+    NUMBER_OF_SIMULATIONS = 1_000_000
     def __init__(self, number_of_dice, number_of_sides):
         self.number_of_dice = number_of_dice
         self.number_of_sides = number_of_sides
