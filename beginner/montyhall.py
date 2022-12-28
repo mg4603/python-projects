c02376eb0fc45687e849a4d6da4b3908df416dbf
Michael George <michael.g.george@gmail.com>
2022-12-28 12:09:32 +0530

    Add display_intro to MontyHall in beginner

--- beginner/montyhall.py
+++ beginner/montyhall.py
@@ -63,3 +63,31 @@ class MontyHall:
                 door_two[i].strip(), 
                 door_three[i].strip()
             ))
+    
+    def display_intro(s):
+        print('------------------------------------------------------------')
+        print('------------------ The Monty Hall Problem ------------------')
+        print('------------------------------------------------------------')
+        print()
+        print('In the Monty Hall game show, you can pick one of three doors.')
+        print('One door has a new car for a prize. The other two doors have')
+        print('worthless goats:')
+        s.display_doors(
+            s.DOOR_CLOSED_ONE, s.DOOR_CLOSED_TWO, s.DOOR_CLOSED_THREE
+        )
+        print('Say you pick Door #1.')
+        print('Before the door you choose is opened, another door with a')
+        print('goat, is opened:')
+        s.display_doors(
+            s.DOOR_CLOSED_ONE, s.DOOR_CLOSED_TWO, s.GOAT_DOOR
+        )
+        print('You can choose to either open the door you originally picked')
+        print('or swap to the other unopened door.')
+        print()
+        print('It may seem like it doesn\'t matter if you swap or not, but')
+        print('your odds do improve if you swap doors! This program')
+        print('demonstrates the Monty Hall problem by letting you do repeated')
+        print('experiments.')
+        print()
+        input('Press Enter to stat...')
+        
