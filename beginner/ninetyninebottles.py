ad7c4a8cf88eb4be777906417ac919ad0f6d45f3
Michael George <michael.g.george@gmail.com>
2022-12-28 15:17:45 +0530

    Add main to NinetyNineBottles in beginner

--- beginner/ninetyninebottles.py
+++ beginner/ninetyninebottles.py
@@ -3,8 +3,9 @@ from time import sleep
 
 class NinetyNineBottles:
     PAUSE = 2
-    def __init__(s, bottles):
+    def __init__(s, bottles = 99):
         s.bottles = bottles
+        assert s.bottles > 0
 
     def display_intro(s):
         print('Ninety-Nine Bottles')
@@ -12,3 +13,32 @@ class NinetyNineBottles:
         print('(Press CTRL-C to quit.)')
         print()
         sleep(2)
+    
+    def main(s):
+        try:
+            while s.bottles > 1:
+                print('{} bottles of milk on the wall,'.format(s.bottles))
+                sleep(s.PAUSE)
+                print('{} bottles of milk,'.format(s.bottles))
+                sleep(s.PAUSE)
+                print('Take one down, pass it around,')
+                sleep(s.PAUSE)
+                s.bottles -= 1
+                if s.bottles == 1:
+                    print('1 bottle of milk on the wall!')
+                    sleep(s.PAUSE)
+                    print()
+                    break
+                print('{} bottles of milk on the wall!'.format(s.bottles))
+                sleep(s.PAUSE)
+                print()
+            
+            print('1 bottle of milk on the wall,')
+            sleep(s.PAUSE)
+            print('1 bottle of milk,')
+            sleep(s.PAUSE)
+            print('Take one down, pass it around,')
+            sleep(s.PAUSE)
+            print('No more bottles of milk on the wall!')
+        except KeyboardInterrupt:
+            exit()
