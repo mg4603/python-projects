b9ac23a309b06a7c9d0dbd805e5ffe6a43257ea3
Michael George <michael.g.george@gmail.com>
2022-12-28 11:43:33 +0530

    Add DOOR_CLOSED_ONE to MontyHall in beginner

--- beginner/montyhall.py
+++ beginner/montyhall.py
@@ -1,3 +1,15 @@
+from random import randint
+from sys import exit
+
 class MontyHall:
+    DOOR_CLOSED_ONE = '''
++------+
+|      |
+|      |
+|   1  |
+|      |
+|      |
++------+
+'''
     def __init__(s):
         pass
