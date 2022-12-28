43e8b13817e2cee748f3e79875722de36a5f5bcf
Michael George <michael.g.george@gmail.com>
2022-12-28 14:37:00 +0530

    Add display_table to MultiplicationTable in beginner

--- beginner/multiplicationtable.py
+++ beginner/multiplicationtable.py
@@ -1,10 +1,27 @@
 class MultiplicationTable:
     def __init__(s, num):
-        s.num = num
-        s.length = len(str(num * num))
+        s.num = num + 1
+        s.length_num = len(str(num))
+        s.offset = len(str(num * num))
     
     def display_intro(s):
         print('----------------------------------------------------')
         print('--------------- Multiplication Table ---------------')
         print('----------------------------------------------------')
+    
+    def display_table(s):
+        top_line = ' ' * s.length_num + '|'
+        for num in range(s.num):
+            top_line += (str(num).rjust(s.offset) + ' ')
         
+        second_line = '-' * (len(top_line) - s.length_num - 1)
+        second_line = '-' * s.length_num + '|' + second_line
+        print()
+        print(top_line)
+        print(second_line)
+        for num1 in range(s.num):
+            print(str(num1).rjust(s.length_num), end= '|')
+            for num2 in range(s.num):
+                print(str(num1 * num2).rjust(s.offset), end=' ')
+            print()
+
