fe14ae52bdfa49190d55c47149a5fbe9494a3775
Michael George <michael.g.george@gmail.com>
2022-12-29 08:17:39 +0530

    Complete ninetyninebottles2 in beginner

--- beginner/ninetyninebottles2.py
+++ beginner/ninetyninebottles2.py
@@ -4,7 +4,7 @@ from time import sleep
 
 class NinetyNineBottles2:
     LINE_PAUSE = 1.5
-    SPEED = 0.01
+    SPEED = .01
     def __init__(s, bottles = 99):
         s.bottles = bottles
         s.lines = [
@@ -24,14 +24,22 @@ class NinetyNineBottles2:
     def effect_one(s):
         line_num = randint(0, 3)
         line = list(s.lines[line_num])
-        char_index = randint(0, len(line) - 1)
+        while True:
+            char_index = randint(0, len(line) - 1)
+            if line[char_index] != ' ':
+                break
+
         line[char_index] = ' '
         s.lines[line_num] = ''.join(line)
     
     def effect_two(s):
         line_num = randint(0, 3)
         line = list(s.lines[line_num])
-        char_index = randint(0, len(line) - 1)
+        while True:
+            char_index = randint(0, len(line) - 1)
+            if line[char_index].isalpha():
+                break
+
         char = line[char_index]
         if char.islower():
             line[char_index] = char.upper()
@@ -42,7 +50,13 @@ class NinetyNineBottles2:
     def effect_three(s):
         line_num = randint(0, 3)
         line = list(s.lines[line_num])
-        char_index = randint(0, len(line) - 2)
+        while True:
+            char_index = randint(0, len(line) - 2)
+            if line[char_index + 1] != ' ' and\
+                    line[char_index] != line[char_index + 1] and\
+                    line[char_index].isalpha():
+                break
+
         line[char_index], line[char_index + 1] = \
             line[char_index + 1], line[char_index]
         s.lines[line_num] = ''.join(line)
@@ -50,30 +64,51 @@ class NinetyNineBottles2:
     def effect_four(s):
         line_num = randint(0, 3)
         line = list(s.lines[line_num])
-        char_index = randint(0, len(line) - 1)
+        while True:
+            char_index = randint(0, len(line) - 1)
+            if line[char_index] != ' ':
+                break
+
         line.insert(char_index, line[char_index])
         s.lines[line_num] = ''.join(line)
     
     def effect_five(s):
         line_num = randint(0, 3)
-        line = s.lines[line_num].split(' ')
+        if s.lines[line_num][0] == ' ':
+            add_space = True
+        else:
+            add_space = False
+        line = s.lines[line_num].strip().split(' ')
         word_index = randint(0, len(line) - 1)
         if line[word_index].isupper():
-            line[word_index].lower()
+            line[word_index] = line[word_index].lower()
         else:
-            line[word_index].upper()
-        s.lines[line_num] = ' '.join(line)
-    
+            line[word_index] = line[word_index].upper()
+        if add_space:
+            s.lines[line_num] = ' ' + ' '.join(line)
+        else:
+            s.lines[line_num] = ' '.join(line)
+
     def effect_six(s):
         line_num = randint(0, 3)
-        line = s.lines[line_num].split(' ')
-        word_index = randint(0, len(line) - 2)
+        if s.lines[line_num][0] == ' ':
+            add_space = True
+        else:
+            add_space = False
+        line = s.lines[line_num].strip().split(' ')
+        while True:
+            word_index = randint(0, len(line) - 2)
+            if line[word_index] != line[word_index + 1]:
+                break
         line[word_index], line[word_index + 1] = \
             line[word_index + 1], line[word_index]
-        s.lines[line_num] = ' '.join(line)
+        if add_space:
+            s.lines[line_num] = ' ' + ' '.join(line)
+        else:
+            s.lines[line_num] = ' '.join(line)
     
     def effect_seven(s):
-        s.upward_count = randint(0, 7)
+        s.upward_count = randint(1, 3)
     
     def slow_print(s, text, pause_amt=0.1):
         for char in text:
@@ -95,13 +130,14 @@ class NinetyNineBottles2:
             else:
                 s.bottles -= 1
             if s.bottles == 0:
-                s.slow_print('No more bottles on the wall!', s.SPEED)
+                s.slow_print('No more' + s.lines[3], s.SPEED)
             else:
-                s.slow_print(str(s.bottles) + s.lines[3], s.sp)
+                s.slow_print(str(s.bottles) + s.lines[3], s.SPEED)
             sleep(s.LINE_PAUSE)
             print()
 
             effect = randint(1, 7)
+            print(effect)
             if effect == 1:
                 s.effect_one()
             elif effect == 2:
@@ -116,9 +152,9 @@ class NinetyNineBottles2:
                 s.effect_six()
             elif effect == 7:
                 s.effect_seven()
-            
+
 if __name__ == '__main__':
-    obj = NinetyNineBottles2()
+    obj = NinetyNineBottles2(99)
     obj.display_intro()
     try:
         obj.main()
