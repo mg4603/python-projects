540a84a8d07feb30e7134306a097d9f5f9fefe95
Michael George <michael.g.george@gmail.com>
2022-12-29 06:46:29 +0530

    Define NinetyNineBottles2 in beginner

--- beginner/ninetyninebottles2.py
+++ beginner/ninetyninebottles2.py
@@ -1,3 +1,13 @@
+from sys import exit
+from random import randint
+from time import sleep
+
+class NinetyNineBottles2:
+    def __init__(s, bottles = 99):
+        s.bottles = bottles
+    
+
+
 if __name__ == '__main__':
     obj = NinetyNineBottles2()
     obj.display_intro()
