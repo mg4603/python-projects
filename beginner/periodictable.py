7a68f4307a34d96029a8e4bc6c18287ef8e44d48
Michael George <michael.g.george@gmail.com>
2022-12-30 06:50:16 +0530

    Add main to PeriodicTable in beginner

--- beginner/periodictable.py
+++ beginner/periodictable.py
@@ -1,17 +1,3 @@
-'''         Periodic Table of Elements
-      1  2  3  4  5  6  7  8  9  10 11 12 13 14 15 16 17 18 
-    1 H                                                  He
-    2 Li Be                               B  C  N  O  F  Ne
-    3 Na Mg                               Al Si P  S  Cl Ar
-    4 K  Ca Sc Ti V  Cr Mn Fe Co Ni Cu Zn Ga Ge As Se Br Kr
-    5 Rb Sr Y  Zr Nb Mo Tc Ru Rh Pd Ag Cd In Sn Sb Te I  Xe
-    6 Cs Ba La Hf Ta W  Re Os Ir Pt Au Hg Tl Pb Bi Po At Rn
-    7 Fr Ra Ac Rf Db Sg Bh Hs Mt Ds Rg Cn Nh Fl Mc Lv Ts Og
-
-            Ce Pr Nd Pm Sm Eu Gd Tb Dy Ho Er Tm Yb Lu
-            Th Pa U  Np Pu Am Cm Bk Cf Es Fm Md No Lr
-    '''
-
 from csv import reader
 from sys import exit
 from pathlib import Path
@@ -67,3 +53,32 @@ class PeriodicTable:
     def display_intro(s):
         print('Periodic Table of Elements')
         print()
+    
+    def main(s):
+        s.setup()
+        while True:
+            print('''
+                Periodic Table of Elements
+        1  2  3  4  5  6  7  8  9  10 11 12 13 14 15 16 17 18 
+        1 H                                                  He
+        2 Li Be                               B  C  N  O  F  Ne
+        3 Na Mg                               Al Si P  S  Cl Ar
+        4 K  Ca Sc Ti V  Cr Mn Fe Co Ni Cu Zn Ga Ge As Se Br Kr
+        5 Rb Sr Y  Zr Nb Mo Tc Ru Rh Pd Ag Cd In Sn Sb Te I  Xe
+        6 Cs Ba La Hf Ta W  Re Os Ir Pt Au Hg Tl Pb Bi Po At Rn
+        7 Fr Ra Ac Rf Db Sg Bh Hs Mt Ds Rg Cn Nh Fl Mc Lv Ts Og
+    
+                Ce Pr Nd Pm Sm Eu Gd Tb Dy Ho Er Tm Yb Lu
+                Th Pa U  Np Pu Am Cm Bk Cf Es Fm Md No Lr
+        ''')
+
+            print('Enter a symbol or atomic number to examine, or QUIT to quit.')
+            response = input('> ').title()
+            if response == 'Quit':
+                exit()
+            
+            if response in s.elements:
+                for key in s.ALL_COLUMNS:
+                    key_justified = key.rjust(s.longest_column)
+                    print('{}: {}'.format(key_justified, s.elements[response][key]))
+                input('Press Enter to continue...')
