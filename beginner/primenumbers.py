4b223984af5c90ef82a561173437943011be13a6
Michael George <michael.g.george@gmail.com>
2023-01-02 19:29:08 +0530

    Add is_prime to PrimeNumbers in beginner

--- beginner/primenumbers.py
+++ beginner/primenumbers.py
@@ -4,3 +4,15 @@ from sys import exit
 class PrimeNumbers:
     def __init__(s):
         pass
+    
+    def is_prime(s, num):
+        if num < 2:
+            return False
+        elif num == 2:
+            return True
+
+        for i in range(2, int(sqrt(num)) + 1):
+            if num % i == 0:
+                return False
+        
+        return True
