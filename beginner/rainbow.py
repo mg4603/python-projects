e95efc8ebe5fa8f4ffeab8ef6f4bfbe6f35a9e58
Michael George <michael.g.george@gmail.com>
2023-01-04 09:18:39 +0530

    Add main to Rainbow in beginner

--- beginner/rainbow.py
+++ beginner/rainbow.py
@@ -14,9 +14,39 @@ class Rainbow:
     VIOLET = 'purple'
 
     def __init__(s):
-        pass
+        s.indent = 0
+        s.indent_increasing = True
 
     def display_intro(s):
         print('Rainbow')
         print('Press CTRL-C to stop.')
         sleep(3)
+    
+    def main(s):
+        while True:
+            print(' ' * s.indent, end='')
+            fg(s.RED)
+            print('##', end='')
+            fg(s.YELLOW)
+            print('##', end='')
+            fg(s.GREEN)
+            print('##', end='')
+            fg(s.BLUE)
+            print('##', end='')
+            fg(s.INDIGO)
+            print('##', end='')
+            fg(s.VIOLET)
+            print('##', end='')
+            print()
+
+            if s.indent_increasing:
+                s.indent += 1
+                if s.indent == 60:
+                    s.indent_increasing = False
+            else:
+                s.indent -= 1
+                if s.indent == 0:
+                    s.indent_increasing = True
+            
+            sleep(0.02)
+            
