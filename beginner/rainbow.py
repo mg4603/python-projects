7dfff6540d1a42e98a5f55e99331b3415b15eba0
Michael George <michael.g.george@gmail.com>
2023-01-04 09:10:47 +0530

    Define Rainbow class in rainbow in beginner

---
+++ beginner/rainbow.py
@@ -0,0 +1,10 @@
+from time import sleep
+from sys import exit
+try:
+    from bext import fg
+except ImportError:
+    exit('This program requires the bext module.')
+
+class Rainbow:
+    def __init__(s):
+        pass
