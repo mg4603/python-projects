b359eac4892e1b3f8c92053898f28b526ca4a38f
Michael George <michael.g.george@gmail.com>
2023-01-06 03:05:05 +0530

    Add get_player_move to rockpaperscissorsalwayswin

--- beginner/rockpaperscissorsalwayswin.py
+++ beginner/rockpaperscissorsalwayswin.py
@@ -30,3 +30,34 @@ class RockPaperScissors:
     def display_stats(s):
         print('Wins: {}, Losses: 0, Ties: 0'.format(s.wins))
     
+    def get_player_move(s):
+        print('{}\n\t{}\n\t{}\n\t{}\n\t{}\n\t{}\n\t{}'.format(
+            'Enter your move:',
+            '(R)ock',
+            '(P)aper',
+            '(S)cissors',
+            '(L)izard',
+            '(Sp)ock',
+            '(Q)uit'
+        ))
+        while True:
+            response = input('> ').upper()
+            if response == 'Q':
+                exit('Thanks for playing!')
+            elif response == 'R':
+                s.player_move = s.ROCK
+                return
+            elif response == 'P':
+                s.player_move = s.PAPER
+                return
+            elif response == 'S':
+                s.player_move = s.SCISSORS
+                return
+            elif response == 'L':
+                s.player_move = s.LIZARD
+                return
+            elif response == 'SP':
+                s.player_move = s.SPOCK
+                return
+            else:
+                print('Type one of R, P, S, Sp or Q.')
