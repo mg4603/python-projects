8c70e38b3557feb379eed2df2d58bc2d4b79157f
Michael George <michael.g.george@gmail.com>
2023-01-05 10:41:49 +0530

    Define RockPaperScissors class in beginner

---
+++ beginner/rockpaperscissors.py
@@ -0,0 +1,9 @@
+from random import randint
+from time import sleep
+from sys import exit
+
+class RockPaperScissors:
+    def __init__(s):
+        s.wins = 0
+        s.losses = 0
+        s.ties = 0
