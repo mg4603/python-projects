89a8d29285d324b973fd90c14aa55f5fe4f6a52c
Michael George <michael.g.george@gmail.com>
2023-01-05 10:48:39 +0530

    Add get_player_move to RockPaperScissors in beginner

--- beginner/rockpaperscissors.py
+++ beginner/rockpaperscissors.py
@@ -6,7 +6,21 @@ class RockPaperScissors:
     ROCK = 'ROCK'
     PAPER = 'PAPER'
     SCISSORS = 'SCISSORS'
+
     def __init__(s):
         s.wins = 0
         s.losses = 0
         s.ties = 0
+    
+    def get_player_move(s):
+        print('Enter your move: (R)ock (P)aper (S)cissors or (Q)uit.')
+        while True:
+            response = input('> ').upper()
+
+            if response == 'Q':
+                exit('Thanks for playing!')
+            elif response in ('R', 'P', 'S'):
+                s.player_move = response
+                return
+            else:
+                print('Enter one of R, P, S or Q.')
