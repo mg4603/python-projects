b668c52cdff15a14b185035b9c8fbd89e2ad29da
Michael George <michael.g.george@gmail.com>
2023-01-05 11:45:47 +0530

    Modify display_player_move in RockPaperScissors
    Added code to handle Lizard and Spock.

--- beginner/rockpaperscissors.py
+++ beginner/rockpaperscissors.py
@@ -135,6 +135,10 @@ class RockPaperScissors:
             print('{} versus...'.format(s.PAPER))
         elif s.player_move == s.SCISSORS:
             print('{} versus...'.format(s.SCISSORS))
+        elif s.player_move == s.LIZARD:
+            print('{} versus...'.format(s.LIZARD))
+        elif s.player_move == s.SPOCK:
+            print('{} versus...'.format(s.SPOCK))
     
     def display_computer_move(s):
         sleep(0.5)
