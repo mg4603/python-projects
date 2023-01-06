f9556674a1a95ed7af439d4b41bdb85514fbf054
Michael George <michael.g.george@gmail.com>
2023-01-07 01:55:57 +0530

    Add translate to Rot13 in rot13cipher in beginner

--- beginner/rot13cipher.py
+++ beginner/rot13cipher.py
@@ -6,5 +6,17 @@ except ImportError:
 class Rot13:
     UPPER_LETTERS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
     LOWER_LETTERS = 'abcdefghijklmnopqrstuvwxyz'
+
     def __init__(s):
         pass
+
+    def translate(s, message):
+        translated = ''
+        for char in message:
+            if char.isupper():
+                translated += s.UPPER_LETTERS[(ord(char) - 65 - 13) % 26]
+            elif char.islower():
+                translated += s.LOWER_LETTERS[(ord(char) - 97 - 13) % 26]
+            else:
+                translated += char
+        return translated
