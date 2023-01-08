9291164c3c33b4c553d79e86701b8bdbb1d8f50f
Michael George <michael.g.george@gmail.com>
2023-01-09 02:53:51 +0530

    Add CARPET_TILE TO TiledPattern in shining_carpet

--- beginner/shining_carpet.py
+++ beginner/shining_carpet.py
@@ -1,5 +1,13 @@
 class TiledPattern:
-    
+    CARPET_TILE = r'''
+_ \ \ \_/ __
+ \ \ \___/ _
+\ \ \_____/ 
+/ / / ___ \_
+_/ / / _ \__
+__/ / / \___
+'''
+
     def __init__(s, num_horizontal_tiles, num_vertical_tiles):
         s.num_horizontal_tiles = num_horizontal_tiles
         s.num_vertical_tiles = num_vertical_tiles
