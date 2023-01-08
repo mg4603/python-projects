7294c6805689b2ec667511e1d044aab160cb2e0e
Michael George <michael.g.george@gmail.com>
2023-01-09 02:53:26 +0530

    Define TiledPattern in shining_carpet in beginner

---
+++ beginner/shining_carpet.py
@@ -0,0 +1,6 @@
+class TiledPattern:
+    
+    def __init__(s, num_horizontal_tiles, num_vertical_tiles):
+        s.num_horizontal_tiles = num_horizontal_tiles
+        s.num_vertical_tiles = num_vertical_tiles
+    
