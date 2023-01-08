272fc6bae3886ee498cc32764963890573f863e3
Michael George <michael.g.george@gmail.com>
2023-01-09 02:58:57 +0530

    Add draw_pattern to TiledPattern in shining_carpet

--- beginner/shining_carpet.py
+++ beginner/shining_carpet.py
@@ -8,7 +8,25 @@ _/ / / _ \__
 __/ / / \___
 '''
 
-    def __init__(s, num_horizontal_tiles, num_vertical_tiles):
+    def __init__(s, num_horizontal_tiles, num_vertical_tiles, tile_type):
         s.num_horizontal_tiles = num_horizontal_tiles
         s.num_vertical_tiles = num_vertical_tiles
-    
+        s.tile_type = tile_type
+
+    def draw_pattern(s):
+        if s.tile_type == 'carpet':
+            tile = s.CARPET_TILE
+        elif s.tile_type == 'brick wall':
+            tile = s.BRICK_WALL_TILE
+        elif s.tile_type == 'parenthesis':
+            tile = s.PARENTHESIS_TILE
+        elif s.tile_type == 'double_hexagon':
+            tile = s.DOUBLE_HEXAGON_TILE
+        elif s.tile_type == 'robot':
+            tile = s.ROBOT
+        elif s.tile_type == 'irregular':
+            tile = s.IRREGULAR
+            
+        for i in range(s.num_vertical_tiles):
+            for line in range(tile.strip().splitlines()):
+                print(line * s.num_horizontal_tiles)
