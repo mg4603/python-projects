d691410331140ffbbd643878d6075b7cc3f3ca0b
Michael George <michael.g.george@gmail.com>
2023-01-10 21:44:23 +0530

    Add display_intro to SubCipher in simplesubcipher

--- beginner/simplesubcipher.py
+++ beginner/simplesubcipher.py
@@ -7,4 +7,10 @@ except ImportError:
 class SubCipher:
     def __init__(s):
         pass
-   
+    
+    def display_intro(s):
+        print('Simple Substitution Cipher')
+        print('A simple substitution cipher has one-to-one translation')
+        print('for each symbol in plaintext and each symbol in the cipher')
+        print('text.')
+
