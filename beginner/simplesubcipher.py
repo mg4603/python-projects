f46afe0630d4fb3563b627e193bb403de87692d6
Michael George <michael.g.george@gmail.com>
2023-01-10 21:46:45 +0530

    Add LETTERS to SubCipher in simplesubcipher in beginner

--- beginner/simplesubcipher.py
+++ beginner/simplesubcipher.py
@@ -5,12 +5,14 @@ except ImportError:
     pass
 
 class SubCipher:
-    def __init__(s):
-        pass
+    LETTERS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    def __init__(s, key):
+        s.key = key
     
     def display_intro(s):
         print('Simple Substitution Cipher')
         print('A simple substitution cipher has one-to-one translation')
         print('for each symbol in plaintext and each symbol in the cipher')
         print('text.')
+
 
