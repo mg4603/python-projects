08479dc326cbbd623b1cd1cb56e35d80f7ddeddd
Michael George <michael.g.george@gmail.com>
2023-01-10 22:06:10 +0530

    Add translated_message to SubCipher in beginner

--- beginner/simplesubcipher.py
+++ beginner/simplesubcipher.py
@@ -6,8 +6,12 @@ except ImportError:
 
 class SubCipher:
     LETTERS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-    def __init__(s, key):
+    def __init__(s, message, key, mode):
+        assert set(list(s.LETTERS)) == set(list(key)), \
+                'There is an error in the key or symbol set.'
         s.key = key
+        s.message = message
+        s.mode = mode
     
     def display_intro(s):
         print('Simple Substitution Cipher')
@@ -16,3 +20,22 @@ class SubCipher:
         print('text.')
 
 
+    def translated_message(s, message, mode):
+        translated = ''
+        charset_a = s.LETTERS
+        charset_b = s.key
+
+        if mode == 'decrypt':
+            charset_a, charset_b = charset_b, charset_b
+        
+        for symbol in message:
+            if symbol.upper() in charset_a:
+                idx = charset_a.find(symbol.upper())
+                if symbol.isupper():
+                    translated += charset_b[idx].upper()
+                elif symbol.islower():
+                    translated += charset_b[idx].lower()
+            else:
+                translated += symbol
+        
+        return translated
