95e6a73383dff99320af2f6b981d4017270d05ad
Michael George <michael.g.george@gmail.com>
2023-01-11 17:17:32 +0530

    Add animate to SineMessage in beginner

--- beginner/sinemessage.py
+++ beginner/sinemessage.py
@@ -5,20 +5,29 @@ from math import sin
 
 class SineMessage:
     WIDTH, HEIGHT = get_terminal_size()
-    STEP = 0.0
     STEP_INC = 0.25 
     def __init__(s, message):
+        s.step = 0.0
         assert 1 <= len(message) <= (s.WIDTH // 2), \
                 'Message must be 1 to {} characters long.'.format(
                     s.WIDTH // 2
                 )
         s.message = message
+        s.multiplier = (s.WIDTH - len(s.message)) // 2
     
     def display_intro():
         print('Sine Message')
         print('Press CTRL-C to quit.')
         print()
     
+    def animate(s):
+        multiplier = (s.WIDTH - len(s.message)) / 2
+        while True:
+            padding = ' ' * int((sin(s.step) + 1) * multiplier)
+            print(padding + s.message)
+            sleep(s.PAUSE)
+            s.step += s.STEP_INC
+
 def get_message():
     WIDTH, _ = get_terminal_size()
     print(
