e48ea73aa34caf0427bbb8bde2e7c3caafafd846
Michael George <michael.g.george@gmail.com>
2023-01-14 15:59:37 +0530

    Add MAX_NUM_SNAILS to SnailRace in beginner

--- beginner/snailrace.py
+++ beginner/snailrace.py
@@ -1,7 +1,15 @@
 from sys import exit
 from random import choice, randint
 from time import sleep
-
+'''
+• Add a random “speed boost” that launches the snail four spaces ahead
+instead of one.
+• Add a “sleep mode” that snails can randomly enter during the race.
+This mode causes them to stop for a few turns and zzz to appear next to
+them.
+• Add support for ties, in case snails reach the finish line at the same time
+'''
 class SnailRace:
+    MAX_NUM_SNAILS = 8
     def __init__(s):
         pass
