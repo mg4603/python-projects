93acf9a579bb0fad3c4a7569f4239883fb53bb93
Michael George <michael.g.george@gmail.com>
2023-01-15 20:31:29 +0530

    Define SudokuGrid class in sudoku in beginner

--- beginner/sudoku/sudoku.py
+++ beginner/sudoku/sudoku.py
@@ -1,6 +1,10 @@
 from sys import exit
 from random import choice
 from copy import copy
+from pathlib import Path
 
+class SudokuGrid:
+    def __init__(s, puzzle):
+        pass
 if __name__ == '__main__':
     main()
