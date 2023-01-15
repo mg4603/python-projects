ccfc0ad9c2dc27f8ceb1eb6db1c1e258c9d3bf12
Michael George <michael.g.george@gmail.com>
2023-01-15 16:00:12 +0530

    Complete display_board in Soroban in beginner

--- beginner/soroban.py
+++ beginner/soroban.py
@@ -20,5 +20,49 @@ class Soroban:
         print('  -a s d f g h j k l ;')
         print('(Enter a number, "quit", or a stream of up/down letters.)')
 
-    def display_board(s):
-        pass
+    def display_board(s, number):
+        number_list = list(str(number).zfill(s.NUM_OF_DIGITS))
+
+        has_bead = []
+        
+        # Top heaven row
+        for i in range(s.NUM_OF_DIGITS):
+            has_bead.append(number_list[i] in '012345679')
+
+        # Bottom heaven row
+        for i in range(s.NUM_OF_DIGITS):
+            has_bead.append(number_list[i] in '012345679')
+
+        # 1st earth row
+        for i in range(s.NUM_OF_DIGITS):
+            has_bead.append(number_list[i] in '012345679')
+
+        # 2nd earth row
+        for i in range(s.NUM_OF_DIGITS):
+            has_bead.append(number_list[i] in '012345679')
+
+        # 3rd earth row
+        for i in range(s.NUM_OF_DIGITS):
+            has_bead.append(number_list[i] in '012345679')
+
+        # 4th earth row
+        for i in range(s.NUM_OF_DIGITS):
+            has_bead.append(number_list[i] in '012345679')
+
+        # 5th earth row
+        for i in range(s.NUM_OF_DIGITS):
+            has_bead.append(number_list[i] in '012345679')
+
+        # 6th earth row
+        for i in range(s.NUM_OF_DIGITS):
+            has_bead.append(number_list[i] in '012345679')
+
+        abacus_chars = []
+        for i, bead_present in enumerate(has_bead):
+            if bead_present:
+                abacus_chars.append('O')
+            else:
+                abacus_chars.append('|')
+        chars = abacus_chars + number_list
+        print(s.board.format(*chars))
+
