e643e0f71e5cb15aedac47321330df6b20a1cf45
Michael George <michael.g.george@gmail.com>
2023-01-15 18:51:29 +0530

    Define SoundMimic in beginner

---
+++ beginner/sound_mimic/sound_mimic.py
@@ -0,0 +1,16 @@
+from random import choice
+from time import sleep
+from sys import exit
+
+try:
+    from playsound import playsound
+except ImportError:
+    print('The playsound module needs to be installed to run this')
+    print('program.')
+    print('use: python3 -m pip install playsound')
+    exit()
+
+
+class SoundMimic:
+    def __init__(s):
+        pass
