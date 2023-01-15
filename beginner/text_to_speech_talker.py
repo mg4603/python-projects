85d73afe8b44c6979b9ccdb2574ee26dabb9cc77
Michael George <michael.g.george@gmail.com>
2023-01-16 03:32:51 +0530

    Add display_intro to text_to_speech_talker in beginner

--- beginner/text_to_speech_talker.py
+++ beginner/text_to_speech_talker.py
@@ -6,6 +6,13 @@ except ImportError:
     print('Use: python3 -m pip install pyttsx3 to install')
     exit()
 
+def display_intro():
+    print('Text To Speech Talker')
+    print()
+    print('Text-to-speech using the pyttsx3 module, which in turn uses')
+    print('the NSSpeechSynthesizer (on macOS), SAPI5 (on Windows),')
+    print('or eSpeak (on Linux) speech engines.')
+    print()
 
 def main():
     engine = pyttsx3.init()
