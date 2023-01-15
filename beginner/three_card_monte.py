393da138067241c4f04a55351e4ee2361149f625
Michael George <michael.g.george@gmail.com>
2023-01-16 04:42:26 +0530

    Define ThreeCardMonte class in beginner

---
+++ beginner/three_card_monte.py
@@ -0,0 +1,14 @@
+from random import choice
+
+class ThreeCardMonte:
+    def __init__(s):
+        pass
+
+    def get_random_card(s):
+        pass
+
+    def has_won(s, guess):
+        pass
+
+    def display_cards(s):
+        pass
