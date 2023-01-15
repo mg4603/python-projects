12b0448ccd0badb37d5a5c1e1a50ac0da7d97974
Michael George <michael.g.george@gmail.com>
2023-01-15 19:13:56 +0530

    Add english_to_spongecae to spongecase in beginner

---
+++ beginner/spongecase.py
@@ -0,0 +1,23 @@
+from random import randint
+try:
+    from pyperclip import copy
+except ImportError:
+    pass
+
+
+def english_to_spongecase(msg):
+    use_upper = False
+    sponge_text = ''
+    for char in msg:
+        if not char.isalpha():
+            sponge_text += char
+        
+        if use_upper:
+            sponge_text += char.upper()
+        else:
+            sponge_text += char.lower()
+
+        if randint(1, 100) <= 90:
+            use_upper = not use_upper
+    
+    return sponge_text
