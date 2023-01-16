72dc2f6d56f81975d652f29a45ec0d915d129749
Michael George <michael.g.george@gmail.com>
2023-01-16 20:23:39 +0530

    Define TowerOfHanoi in towerofhanoi in beginner

---
+++ beginner/towerofhanoi.py
@@ -0,0 +1,15 @@
+class TowerOfHanoi:
+    def __init__(s):
+        pass
+
+    def display_towers(s):
+        pass
+
+    def _display_disk(s):
+        pass
+
+    def check_won(s):
+        pass
+
+    def make_move(s):
+        pass
