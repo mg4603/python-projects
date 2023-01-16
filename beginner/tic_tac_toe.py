b5a9af25bc364159069dde6d4cb7e29e8040794b
Michael George <michael.g.george@gmail.com>
2023-01-16 17:40:36 +0530

    Define TicTacToeBoard in tic_tac_toe in beginner

---
+++ beginner/tic_tac_toe.py
@@ -0,0 +1,20 @@
+class TicTacToeBoard:
+    def __init__(s):
+        pass
+    
+    def is_winner(s):
+        pass
+
+    def is_full(s):
+        pass
+
+    def display(s):
+        pass
+
+    def get_blank_board(s):
+        pass
+
+    def make_move(s):
+        pass
+
+    
