49fc6add649c3f72680ddfa81b28a383d3a2f517
Michael George <michael.g.george@gmail.com>
2023-01-16 20:26:49 +0530

    Add display_intro to towerofhanoi in beginner

--- beginner/towerofhanoi.py
+++ beginner/towerofhanoi.py
@@ -1,3 +1,6 @@
+from copy import copy
+from sys import exit
+
 class TowerOfHanoi:
     def __init__(s):
         pass
@@ -13,6 +16,13 @@ class TowerOfHanoi:
 
     def make_move(s):
         pass
+
+def display_intro():
+    print('The Tower of Hanoi')
+    print()
+    print('Move the tower of disks, one disk at a time, to another target.')
+    print('Larger disks cannot rest on top of a smaller disk.')
+
 
 if __name__ == '__main__':
     main()
