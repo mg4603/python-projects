697d9b86aa4a9cda8522fbdce299245b3518efd0
Michael George <michael.g.george@gmail.com>
2023-01-17 05:59:27 +0530

    Define Twenty48Board in twenty_fortyeight in beginner

--- beginner/twenty_fortyeight.py
+++ beginner/twenty_fortyeight.py
@@ -1,7 +1,27 @@
 from random import randint
 from sys import exit
 
+class Twenty48Board:
+    def __init__(s):
+        pass
 
+    def get_new_board(s):
+        pass
+    
+    def display(s):
+        pass
+
+    def _combine_columns(s):
+        pass
+
+    def make_move(s):
+        pass
+
+    def add_two_to_board(s):
+        pass
+
+    def is_full(s):
+        pass
 
 if __name__ == '__main__':
     main()
