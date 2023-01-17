ebfe4c8a9fd4b7bf8c7542c2c4e373860232aef3
Michael George <michael.g.george@gmail.com>
2023-01-17 20:18:13 +0530

    Add main to vigener in beginner

--- beginner/vigener.py
+++ beginner/vigener.py
@@ -3,6 +3,31 @@ try:
 except ImportError:
     pass
 
+def main():
+    display_intro()
+
+    mode = get_mode()
+    
+    key = get_key()
+
+    cipher = Vigenere(key)
+
+    print('Enter message to {}.'.format(mode))
+    message = input('> ')
+
+    if mode == 'encrypt':
+        translated = cipher.encrypt(message)
+    elif mode == 'decrypt':
+        translated = cipher.decrypt(message)
+    
+    print('{}ed message:'.format(mode.title()))
+    print(translated)
+
+    try:
+        copy(translated)
+        print('({}ed copied to clipboard.)'.format(mode))
+    except NameError:
+        pass
 
 if __name__ == '__main__':
     main()
