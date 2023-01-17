2b1948744b00b1c7c15970633cfe9b64bd1d6c40
Michael George <michael.g.george@gmail.com>
2023-01-18 01:09:25 +0530

    Add get_bucket to waterbucket in beginner

--- beginner/waterbucket.py
+++ beginner/waterbucket.py
@@ -23,7 +23,16 @@ class WaterBuckets:
     def reached_goal(s):
         pass
 
-
+def get_bucket(bucket_sizes):
+    assert len(bucket_sizes) == 3, 'Expected list of sizes of 3 buckets'
+    print('Select a bucket {} or QUIT'.format(', '.join(bucket_sizes)))
+    while True:
+        response = input('> ').upper().strip()
+        if response == 'QUIT':
+            exit('Thanks for playing!')
+        elif response in bucket_sizes:
+            return response
+        print('Please enter one of "{}" or QUIT'.format('", "'.join(bucket_sizes)))
 
 if __name__ == '__main__':
     main()
