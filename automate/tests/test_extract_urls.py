f2342012f3da89cf416eda19ef5c0f5c980f6a8a
Michael George <michael.g.george@gmail.com>
2023-01-19 20:35:08 +0530

    Remove automate dir

--- automate/tests/test_extract_urls.py
+++
@@ -1,9 +0,0 @@
-from scripts.extract_urls import url_extractor
-
-def test_url_extractor():
-    assert url_extractor('http://google.com') == 'http://google.com'
-    assert url_extractor('https://accounts.google.com') == 'https://accounts.google.com'
-    assert url_extractor('http://google.com https://accounts.google.com') \
-        == 'http://google.com\nhttps://accounts.google.com'
-    assert url_extractor('https://accounts.google.edu.uk') == 'https://accounts.google.edu.uk'
-    
--- automate/tests/test_strip.py
+++
@@ -1,6 +0,0 @@
-from scripts.strip import strip
-
-def test_strip():
-    assert strip(' asfd') == 'asfd'
-    assert strip('asdf', 'a') == 'sdf'
-    assert strip('asdf', 'af') == 'sd'
