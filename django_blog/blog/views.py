c240d72134d3295ba65ccc9362beeddec11bac5e
Michael George <michael.g.george@gmail.com>
2024-02-04 09:11:48 +0530

    Add about view to blog in django_blog

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -4,3 +4,6 @@ from django.http import HttpResponse
 # Create your views here.
 def home(request):
     return HttpResponse('<h1>Blog home </h1>')
+
+def about(request):
+    return HttpResponse('<h1> Blog About </h1>')
