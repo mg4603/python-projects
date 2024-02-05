9d896e435e55b8d8d82215797c92fca7b6fdb1b2
Michael George <michael.g.george@gmail.com>
2024-02-05 21:45:19 +0530

    Modify blog home to use base template in django_blog

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,9 +1,26 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-# Create your views here.
+posts = [
+    {
+        'author': 'John Doe',
+        'title': 'Post 1',
+        'date_posted': 'Feb 5, 2024',
+        'content': "Content of Post1"
+    },
+    {
+        'author': 'Jane Doe',
+        'title': 'Post 2',
+        'date_posted': 'Feb 5, 2024',
+        'content': "Content of Post2"
+    }
+]
+
 def home(request):
-    return render(request, 'blog/home.html')
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
 
 def about(request):
     context = {
