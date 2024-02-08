24030aba9eb84ffcf3f3449b31056c9131abcb69
Michael George <michael.g.george@gmail.com>
2024-02-08 22:36:27 +0530

    Add login route to django_blog

--- django_blog/django_project/urls.py
+++ django_blog/django_project/urls.py
@@ -16,10 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.auth import views as auth_views
 from users import views as users_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('register/', users_view.register),
+    path('register/', users_view.register, name='register'),
+    path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
     path('', include('blog.urls'))
 ]
